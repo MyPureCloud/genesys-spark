5b837a1c09daeede2a017e7fcd16354af4b01f82
thomas PARIAUD <thomas.pariaud@genesys.com>
2018-10-23 15:58:29 +0200

    [COMUI-7] UX Interactive Element: Action Button first draft

---
+++ src/components/rich/genesys-action-button/tests/genesys-action-button.spec.ts
@@ -0,0 +1,7 @@
+import { GenesysActionButton } from '../genesys-action-button';
+
+describe('genesys-action-button', () => {
+  it('builds', () => {
+    expect(new GenesysActionButton()).toBeTruthy();
+  });
+});
