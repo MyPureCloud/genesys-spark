e77526e456b687ec00907a0313631991eea90cd7
thomas PARIAUD <thomas.pariaud@genesys.com>
2018-10-23 11:22:36 +0200

    [COMUI-9] UX Interactive Element: Buttons

--- src/components/global/genesys-button/genesys-button.tsx
+++ src/components/global/genesys-button/genesys-button.tsx
@@ -13,20 +13,20 @@ export class GenesysButton {
   disabled: boolean = false;
 
   /**
-   * The component type (secondary or primary).
+   * The component accent (secondary or primary).
    */
   @Prop() 
-  type: string = 'secondary';
+  accent: string = 'secondary';
 
   
   @State()
   title: string;
 
   /**
-   * This function is to check type and return a default one
+   * This function is to check color and return a default one
    */
-  getType() {
-    return this.type === 'primary' ? 'primary' : 'secondary'
+  getAccent() {
+    return this.accent === 'primary' ? 'primary' : 'secondary'
   }
 
   componentDidLoad () {
@@ -37,7 +37,7 @@ export class GenesysButton {
     return (
       <button title={this.title} 
               disabled={this.disabled} 
-              class={'genesys-' + this.getType()}>
+              class={'genesys-' + this.getAccent()}>
         <slot></slot>
       </button>
     );
--- src/components/global/genesys-button/tests/genesys-button.spec.ts
+++ src/components/global/genesys-button/tests/genesys-button.spec.ts
@@ -5,22 +5,22 @@ describe('genesys-button', () => {
     expect(new GenesysButton()).toBeTruthy();
   });
 
-  describe('getType', () => {
-    it('returns secondary string for no type defined', () => {
+  describe('getAccent', () => {
+    it('returns secondary string for no accent defined', () => {
       const component = new GenesysButton();
-      expect(component.getType()).toEqual('secondary');
+      expect(component.getAccent()).toEqual('secondary');
     });
 
-    it('returns primary if type equals to primary', () => {
+    it('returns primary if accent equals to primary', () => {
       const component = new GenesysButton();
-      component.type = 'primary';
-      expect(component.getType()).toEqual('primary');
+      component.accent = 'primary';
+      expect(component.getAccent()).toEqual('primary');
     });
 
-    it('returns secondary string for other types defined', () => {
+    it('returns secondary string for other accents defined', () => {
       const component = new GenesysButton();
-      component.type = 'sss';
-      expect(component.getType()).toEqual('secondary');
+      component.accent = 'sss';
+      expect(component.getAccent()).toEqual('secondary');
     });
 
   });
