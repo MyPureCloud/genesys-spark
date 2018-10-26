d93c7b747bd492351c4599d79649b42a94431a5b
thomas PARIAUD <thomas.pariaud@genesys.com>
2018-10-26 16:10:59 +0200

    [COMUI-27] - create common enum file

--- src/components/global/genesys-rating/genesys-rating.tsx
+++ src/components/global/genesys-rating/genesys-rating.tsx
@@ -1,15 +1,5 @@
 import { Component, Element, Event, EventEmitter, Prop, State } from '@stencil/core';
-
-enum KeyCode {
-  Up = 38,
-  Down = 40,
-  Left = 37,
-  Right = 39,
-  Enter = 13,
-  Space = 32,
-  Home = 36,
-  End = 35
-}
+import { KeyCode } from '../../../common-enum';
 
 @Component({
   styleUrl: 'genesys-rating.less',
