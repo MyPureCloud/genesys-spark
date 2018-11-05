e7fc30d32711bc353b59b81e208c2d69d487ec49
thomas PARIAUD <thomas.pariaud@genesys.com>
2018-11-05 10:09:18 +0100

    remove old files

--- src/components/global/genesys-action-button/stories/story.tsx
+++ src/components/global/genesys-action-button/stories/story.tsx
@@ -3,7 +3,7 @@ import { object, select, text, withKnobs } from '@storybook/addon-knobs/polymer'
 import { storiesOf } from '@storybook/polymer';
 import { withReadme  } from 'storybook-readme';
 
-import README from 'MD/genesys-action-button/README.md';
+import README from '../readme.md';
 
 storiesOf('Genesys Components', module)
 .addDecorator(withKnobs)
