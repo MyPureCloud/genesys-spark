5b27ae3a4f8dd230090a06ea1046faf1899efbf8
thomas PARIAUD <thomas.pariaud@genesys.com>
2018-11-05 10:09:18 +0100

    remove old files

--- src/components/global/genesys-list/README.md
+++
@@ -1,3 +0,0 @@
-# genesys-list
-
-This component allow to display a list in the correct way.
--- src/components/global/genesys-list/stories/story.tsx
+++ src/components/global/genesys-list/stories/story.tsx
@@ -3,7 +3,7 @@ import { array, object, select, withKnobs } from '@storybook/addon-knobs/polymer
 import { storiesOf } from '@storybook/polymer';
 import { withReadme  } from 'storybook-readme';
 
-import README from 'MD/genesys-list/README.md';
+import README from '../readme.md';
 
 storiesOf('Genesys Components', module)
 .addDecorator(withKnobs)
