9523fb12850f42d47c08c59138ab2845dd7a5391
Keri Lawrence <keri.lawrence@genesys.com>
2018-11-06 17:54:57 -0500

    [COMUI-23] get tests passing

--- src/components/global/genesys-slider/tests/genesys-slider.e2e.ts
+++ src/components/global/genesys-slider/tests/genesys-slider.e2e.ts
@@ -9,24 +9,14 @@ describe('genesys-slider', () => {
     expect(element).toHaveClass('hydrated');
   });
 
-  it('renders changes to the name data', async () => {
+  it('renders changes when value is changed', async () => {
     const page = await newE2EPage();
 
     await page.setContent('<genesys-slider></genesys-slider>');
     const component = await page.find('genesys-slider');
-    const element = await page.find('genesys-slider >>> div');
-    expect(element.textContent).toEqual(`Hello, World! I'm `);
-
-    component.setProperty('first', 'James');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James`);
-
-    component.setProperty('last', 'Quincy');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James Quincy`);
-
-    component.setProperty('middle', 'Earl');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James Earl Quincy`);
+    const input = await page.find('genesys-slider input');
+    expect(input.getAttribute('value')).toEqual(0);
+    component.setProperty('value', 5);
+    expect(input.getAttribute('value')).toEqual(5);
   });
 });
