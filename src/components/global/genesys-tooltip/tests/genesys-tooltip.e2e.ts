78b6cecdd3fd894e88f52dfcfe6d78012c97e158
Jérémie Pichon <jeremie.pichon@outlook.fr>
2018-11-12 18:05:09 +0100

    fix unit-tests

--- src/components/global/genesys-tooltip/tests/genesys-tooltip.e2e.ts
+++ src/components/global/genesys-tooltip/tests/genesys-tooltip.e2e.ts
@@ -8,25 +8,4 @@ describe('genesys-tooltip', () => {
     const element = await page.find('genesys-tooltip');
     expect(element).toHaveClass('hydrated');
   });
-
-  it('renders changes to the name data', async () => {
-    const page = await newE2EPage();
-
-    await page.setContent('<genesys-tooltip></genesys-tooltip>');
-    const component = await page.find('genesys-tooltip');
-    const element = await page.find('genesys-tooltip >>> div');
-    expect(element.textContent).toEqual(`Hello, World! I'm `);
-
-    component.setProperty('first', 'James');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James`);
-
-    component.setProperty('last', 'Quincy');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James Quincy`);
-
-    component.setProperty('middle', 'Earl');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James Earl Quincy`);
-  });
 });
--- src/components/global/genesys-tooltip/tests/genesys-tooltip.spec.ts
+++ src/components/global/genesys-tooltip/tests/genesys-tooltip.spec.ts
@@ -1,35 +1,19 @@
 import { GenesysTooltip } from '../genesys-tooltip';
 
 describe('genesys-tooltip', () => {
+  let component;
+  beforeEach(async () => {
+    component = new GenesysTooltip();
+  });
   it('builds', () => {
-    expect(new GenesysTooltip()).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  describe('formatting', () => {
-    it('returns empty string for no names defined', () => {
-      const component = new GenesysTooltip();
-      expect(component.format()).toEqual('');
-    });
-
-    it('formats just first names', () => {
-      const component = new GenesysTooltip();
-      component.first = 'Joseph';
-      expect(component.format()).toEqual('Joseph');
-    });
-
-    it('formats first and last names', () => {
-      const component = new GenesysTooltip();
-      component.first = 'Joseph';
-      component.last = 'Publique';
-      expect(component.format()).toEqual('Joseph Publique');
-    });
-
-    it('formats first, middle and last names', () => {
-      const component = new GenesysTooltip();
-      component.first = 'Joseph';
-      component.middle = 'Quincy';
-      component.last = 'Publique';
-      expect(component.format()).toEqual('Joseph Quincy Publique');
+  describe('methods', () => {
+    it('resize', () => {
+      spyOn(component, 'setPosition');
+      component.resize();
+      expect(component.setPosition).toHaveBeenCalled();
     });
   });
 });
