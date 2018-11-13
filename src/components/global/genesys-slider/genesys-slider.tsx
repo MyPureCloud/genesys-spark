71fd9a1d4b2f0f985712158d8565ab13fa7734bd
Keri Lawrence <keri.lawrence@genesys.com>
2018-11-13 14:35:00 -0500

    [COMUI-23] clean up code

--- src/components/global/genesys-slider/genesys-slider.tsx
+++ src/components/global/genesys-slider/genesys-slider.tsx
@@ -8,23 +8,28 @@ export class GenesysSlider {
   /**
    * Indicates the minimum value for the slider
    **/
-  @Prop({ mutable: true}) min: number = 0;
+  @Prop({ mutable: true })
+  min: number = 0;
   /**
    * Indicates the maximum value for the slider
    **/
-  @Prop() max: number = 100;
+  @Prop()
+  max: number = 100;
   /**
    * Indocates the value of the slider
    **/
-  @Prop({ mutable: true, reflectToAttr: true }) value: number = 0;
+  @Prop({ mutable: true, reflectToAttr: true })
+  value: number = 0;
   /**
    * Indicate if the input box
    **/
-  @Prop() displayTextBox: boolean = false;
+  @Prop()
+  displayTextBox: boolean = false;
   /**
    * Indicate if the value is a percentage
    **/
-  @Prop() isPercentage: boolean = false;
+  @Prop()
+  isPercentage: boolean = false;
 
   sliderInput: HTMLInputElement;
   sliderMask: HTMLElement;
@@ -34,9 +39,10 @@ export class GenesysSlider {
    * Triggered when the value is changed
    * @return the current value
    */
-  @Event() update: EventEmitter;
+  @Event()
+  update: EventEmitter;
 
-  updateValue (event) {
+  updateValue(event) {
     const regex = new RegExp('([0-9]+)%*');
     const result = regex.exec(event.target.value);
     var newValue = result && result[1] ? +result[1] : this.min;
@@ -56,63 +62,60 @@ export class GenesysSlider {
   /**
    * Once the component is loaded do the setup
    */
-  componentDidLoad () {
+  componentDidLoad() {
     this.updatePosition();
   }
 
   /**
-  * When position is changed, via slider or text box, update position
-  */
+   * When position is changed, via slider or text box, update position
+   */
   updatePosition() {
     const width = this.sliderInput.offsetWidth;
-    const placementPercentage = (this.value - this.min)/(this.max - this.min);
+    const placementPercentage = (this.value - this.min) / (this.max - this.min);
     if (this.sliderTooltip) {
       var thumbOffset = -18 * (placementPercentage - 0.5) + 6;
-      var newPosition = (placementPercentage * width) + thumbOffset - 25;
-      this.sliderTooltip.style.left = newPosition + 'px';
+      var newPosition = placementPercentage * width + thumbOffset - 25;
+      this.sliderTooltip.style.left = `${newPosition}px`;
     }
     thumbOffset = -12 * (placementPercentage - 0.5) + 6;
-    newPosition = (placementPercentage * width) + thumbOffset;
-    this.sliderMask.style.width = newPosition + 'px';
+    newPosition = placementPercentage * width + thumbOffset;
+    this.sliderMask.style.width = `${newPosition}px`;
   }
 
   render() {
     const value = this.isPercentage ? `${this.value}%` : this.value;
-    return (<div class="container">
-      <input
-        type="range"
-        role="slider"
-        class="range-input"
-        min={this.min}
-        max={this.max}
-        value={this.value}
-        aria-valuemin={this.min}
-        aria-valuemax={this.max}
-        aria-valuenow={this.value}
-        ref={el => this.sliderInput = el}
-        onInput={(e: UIEvent) => this.updateValue(e)}>
-      </input>
-      <div
-        class="mask"
-        ref={el => this.sliderMask = el}>
-      </div>
-      {this.displayTextBox ? (
+    return (
+      <div class="container">
         <input
-          type="text"
-          class="text-input small-body"
-          value={value}
-          onChange={(e: UIEvent) => this.updateValue(e)}>
-        </input>
-      ) : (
-        <div
-          class="range-tooltip small-body"
-          ref={el => this.sliderTooltip = el}>
+          type="range"
+          role="slider"
+          class="range-input"
+          min={this.min}
+          max={this.max}
+          value={this.value}
+          aria-valuemin={this.min}
+          aria-valuemax={this.max}
+          aria-valuenow={this.value}
+          ref={el => (this.sliderInput = el)}
+          onInput={(e: UIEvent) => this.updateValue(e)}
+        />
+        <div class="mask" ref={el => (this.sliderMask = el)} />
+        {this.displayTextBox ? (
+          <input
+            type="text"
+            class="text-input small-body"
+            value={value}
+            onChange={(e: UIEvent) => this.updateValue(e)}
+          />
+        ) : (
           <div
-             class="tooltip-value">
-             {value}
-         </div>
-        </div>
+            class="range-tooltip small-body"
+            ref={el => (this.sliderTooltip = el)}
+          >
+            <div class="tooltip-value">{value}</div>
+          </div>
         )}
-    </div>);
+      </div>
+    );
   }
 }
