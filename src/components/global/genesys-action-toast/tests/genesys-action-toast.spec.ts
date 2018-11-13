70b42a10e2b1ee383cef939bb80a12d68ed3e6d7
thomas PARIAUD <thomas.pariaud@genesys.com>
2018-11-13 17:14:19 +0100

    [COMUI-26] - UX Interactive Element: Toast

---
+++ src/components/global/genesys-action-toast/tests/genesys-action-toast.spec.ts
@@ -0,0 +1,7 @@
+import { GenesysActionToast } from '../genesys-action-toast';
+
+describe('genesys-action-toast', () => {
+  it('builds', () => {
+    expect(new GenesysActionToast()).toBeTruthy();
+  });
+});
