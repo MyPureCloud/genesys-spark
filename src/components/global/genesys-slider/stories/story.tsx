da9be1d96d5749ac30317eace087fefcbd6ddf16
Keri Lawrence <keri.lawrence@genesys.com>
2019-01-09 14:15:53 -0500

    [COMUI-23] reduce the hight

--- src/components/global/genesys-slider/stories/story.tsx
+++ src/components/global/genesys-slider/stories/story.tsx
@@ -1,22 +1,43 @@
 import { action } from '@storybook/addon-actions';
-import { boolean, number, select, withKnobs } from '@storybook/addon-knobs/polymer';
+import {
+  boolean,
+  number,
+  select,
+  withKnobs
+} from '@storybook/addon-knobs/polymer';
 import { storiesOf } from '@storybook/polymer';
-import { withReadme  } from 'storybook-readme';
+import { html, render } from 'lit-html';
+import { withReadme } from 'storybook-readme';
 
 import README from '../readme.md';
 
 storiesOf('Genesys Components', module)
-.addDecorator(withKnobs)
-.add(
+  .addDecorator(withKnobs)
+  .add(
     'Genesys Slider',
     withReadme(README, () => {
-      const el = document.createElement('genesys-slider');
-      el.displayTextBox = boolean('displayTextBox', true);
-      el.min = number('min', 0);
-      el.max = number('max', 10);
-      el.isPercentage = boolean('isPercentage', true);
-      el.addEventListener('update', e => action('update')(e.detail))
-      document.getElementsByTagName('html')[0].className = 'genesys-' + select('theme', ['dark', 'default'], 'default') + '-theme';
+      const el = document.createElement('div');
+      render(
+        html`
+      <div style="padding:50px 25px 0px 25px;">
+        <genesys-slider
+          id="interactive"
+          min=${number('min', 0)}
+          max=${number('max', 10)}
+          value=${number('value', 3)}
+          isPercentage=${boolean('isPercentage', true)}
+          displayTextBox=${boolean('displayTextBox', true)}
+          >
+        </genesys-slider>
+      </div>`,
+        el
+      );
+      setTimeout(() => {
+        const slider = document.getElementById('interactive');
+        slider.addEventListener('update', e => action('update')(e.detail));
+      }, 100);
+      document.getElementsByTagName('html')[0].className =
+        'genesys-' + select('theme', ['dark', 'default'], 'default') + '-theme';
       return el;
     })
-);
+  );
