8439351778eb7c0f4d32e02c4b8c5f256e93b21f
Reginald Cui <reginald.cui@genesys.com>
2019-04-23 14:53:06 -0400

    test(comui-18): add tests for determining what parts of the farme is rendered

--- src/components/global/gux-panel-frame/tests/gux-panel-frame.e2e.ts
+++ src/components/global/gux-panel-frame/tests/gux-panel-frame.e2e.ts
@@ -8,4 +8,28 @@ describe('gux-panel-frame', () => {
     const element = await page.find('gux-panel-frame');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('should render all parts', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<gux-panel-frame><div slot="header"></div><div slot="body"></div><div slot="footer"></div></gux-panel-frame>');
+    const header = await page.find('.gux-panel-header');
+    const body = await page.find('.gux-panel-body');
+    const footer = await page.find('.gux-panel-footer');
+    expect(header).toBeTruthy();
+    expect(body).toBeTruthy();
+    expect(footer).toBeTruthy();
+  });
+
+  it('should not render any part', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<gux-panel-frame><div slot="nothing"></div></gux-panel-frame>');
+    const header = await page.find('.gux-panel-header');
+    const body = await page.find('.gux-panel-body');
+    const footer = await page.find('.gux-panel-footer');
+    expect(header).toBeFalsy();
+    expect(body).toBeFalsy();
+    expect(footer).toBeFalsy();
+  });
 });
