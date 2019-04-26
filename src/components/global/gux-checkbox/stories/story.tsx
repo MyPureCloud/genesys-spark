3a7212a24cd58e264cc5a50b52d3c5dfed8a6fb8
Christopher Covert <covertops5@gmail.com>
2019-04-26 09:12:00 -0400

    fix(gux-checkbox): linting issues

--- src/components/global/gux-checkbox/stories/story.tsx
+++ src/components/global/gux-checkbox/stories/story.tsx
@@ -1,31 +1,17 @@
 import { checkA11y } from '@storybook/addon-a11y';
 import { action } from '@storybook/addon-actions';
-import { boolean, select, text, withKnobs } from '@storybook/addon-knobs/polymer';
+import {
+  boolean,
+  select,
+  text,
+  withKnobs
+} from '@storybook/addon-knobs/polymer';
 import { storiesOf } from '@storybook/polymer';
 import { withReadme } from 'storybook-readme';
 
-import README from '../readme.md';
 import { GuxCheckbox } from '../gux-checkbox';
 
-const createCheckbox = (
-  label: string,
-  checked: boolean | null | 'undefined'
-) => {
-  const cb = document.createElement('gux-checkbox');
-  cb.label = label;
-
-  if (checked === undefined || checked === 'undefined') {
-    cb.checked = undefined;
-  } else {
-    cb.checked = checked === null ? undefined : checked;
-  }
-
-  cb.addEventListener('checkedChanged', (e: any) =>
-    action('checkedChanged')(e.detail)
-  );
-
-  return cb;
-};
+import README from '../readme.md';
 
 storiesOf('Genesys Components', module)
   .addDecorator(withKnobs)
@@ -43,10 +29,12 @@ storiesOf('Genesys Components', module)
           <gux-checkbox id="sandwich-checkbox" label="Sandwich" disabled="true"></gux-checkbox>
       `;
 
-      const cb = container.querySelector('#sandwich-checkbox') as any as GuxCheckbox;
+      const cb = (container.querySelector(
+        '#sandwich-checkbox'
+      ) as any) as GuxCheckbox;
       cb.label = text('label', 'Sandwich');
-      cb.checked = boolean('checked')
-      cb.indeterminate = boolean('indeterminate')
+      cb.checked = boolean('checked');
+      cb.indeterminate = boolean('indeterminate');
       cb.disabled = boolean('disabled', true);
 
       [
