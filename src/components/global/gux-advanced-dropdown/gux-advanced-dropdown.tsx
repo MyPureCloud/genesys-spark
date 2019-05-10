9d55a7316b59641b25ced1004dbab69a7d1cd836
Jarrod Stormo <stormojm@gmail.com>
2019-05-10 16:33:30 +0200

    feat(advanced-dropdown): basic feature/control implementation

--- src/components/global/gux-advanced-dropdown/gux-advanced-dropdown.tsx
+++ src/components/global/gux-advanced-dropdown/gux-advanced-dropdown.tsx
@@ -1,51 +1,140 @@
-import { Component, Event, EventEmitter, Prop } from '@stencil/core';
+import { Component, Element, Listen, Prop, State } from '@stencil/core';
+import { KeyCode } from '../../../common-enums';
+import { IListItem } from '../../../common-interfaces';
 
 @Component({
   styleUrl: 'gux-advanced-dropdown.less',
   tag: 'gux-advanced-dropdown'
 })
 export class GuxAdvancedDropdown {
-  /**
-   * Indicate the first
-   */
-  @Prop()
-  first: string;
-  /**
-   * Indicate the middle
-   */
-  @Prop()
-  middle: string;
-  /**
-   * Indicate the last
-   */
-  @Prop()
-  last: string;
+  @Element()
+  root: HTMLStencilElement;
 
   /**
-   * Triggered 2s after the component is loaded.
-   * @return the current fullname
+   * The list items, an item contains a `text` and can be disabled.
    */
-  @Event()
-  custom: EventEmitter;
-  emitEvent() {
-    this.custom.emit(this.format());
-  }
+  @Prop()
+  items: IListItem[] = [];
 
-  componentDidLoad() {
-    setTimeout(() => {
-      this.emitEvent();
-    }, 2000);
-  }
+  /**
+   * Disable the input and prevent interactions.
+   */
+  @Prop()
+  disabled: boolean = false;
 
-  format(): string {
-    return (
-      (this.first || '') +
-      (this.middle ? ` ${this.middle}` : '') +
-      (this.last ? ` ${this.last}` : '')
-    );
+  /**
+   * The dropdown label.
+   */
+  @Prop()
+  label: string;
+
+  /**
+   * The dropdown placeholder.
+   */
+  @Prop()
+  placeholder: string;
+
+  @State()
+  opened: boolean;
+
+  @State()
+  value: string;
+
+  @State()
+  searchInput: string = '';
+
+  @Listen('focusout')
+  onFocusOut(e: FocusEvent) {
+    if (!e.relatedTarget || !this.root.contains(e.relatedTarget as Node)) {
+      this.opened = false;
+    }
   }
 
   render() {
-    return <div>Hello, World! I'm {this.format()}</div>;
+    return (
+      <div
+        class={`gux-dropdown ${this.disabled ? 'disabled' : ''} ${
+          this.opened ? 'active' : ''
+        }`}
+      >
+        {this.label && <label>{this.label}</label>}
+        <div class="select-field">
+          <a
+            class="select-input"
+            tabindex="0"
+            onMouseDown={() => {
+              this._clickHandler();
+            }}
+            onKeyDown={e => this._onKeyDown(e)}
+          >
+            {this.placeholder &&
+              !this.value && (
+                <span class="select-placeholder">{this.placeholder}</span>
+              )}
+            {this.value && <span class="select-value">{this.value}</span>}
+          </a>
+          <button
+            aria-hidden="true"
+            tabindex="-1"
+            type="button"
+            class="genesys-icon-dropdown-arrow"
+          />
+        </div>
+        <div class={`advanced-dropdown-menu ${this.opened ? 'opened' : ''}`}>
+          <div class="dropdown-menu-container">
+            <div class="search">
+              <gux-text-field
+                onInput={e => {
+                  this._inputHandler(e);
+                }}
+              />
+              <i aria-hidden="true" class="genesys-icon-search" />
+            </div>
+            <gux-list
+              onChange={e => {
+                this._setValue(e.detail);
+              }}
+              items={this.filteredItems}
+              highlight={this.searchInput}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  private _setValue(text: string) {
+    this.value = text;
+    this.opened = false;
+  }
+
+  private _clickHandler() {
+    if (this.disabled) {
+      return;
+    }
+    this.opened = !this.opened;
+  }
+
+  private _onKeyDown(event: KeyboardEvent) {
+    switch (event.keyCode) {
+      case KeyCode.Up:
+      case KeyCode.Down:
+      case KeyCode.Space:
+        this.opened = true;
+        break;
+      default:
+    }
+  }
+
+  private _inputHandler(event: CustomEvent) {
+    this.searchInput = event.detail;
+  }
+
+  private get filteredItems() {
+    const arr = this.items.filter(item => {
+      return item.text.toLowerCase().startsWith(this.searchInput.toLowerCase());
+    });
+
+    return arr;
   }
 }
