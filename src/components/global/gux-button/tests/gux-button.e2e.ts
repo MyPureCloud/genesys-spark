61d0ecff794fdc4e268448a0ed59a19d776e298e
Mohammed Zaghloul <Mohammed.Zaghloul@genesys.com>
2019-06-19 17:46:51 +0200

    test(button): add e2e test for click event firing while disabled
    add the following e2e tests:
    - Should fire click event if not disabled
    - Should not fire click event if disabled
    
    closes issue COMUI-106

--- src/components/global/gux-button/tests/gux-button.e2e.ts
+++ src/components/global/gux-button/tests/gux-button.e2e.ts
@@ -7,4 +7,26 @@ describe('gux-button', () => {
     const element = await page.find('gux-button');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('Should fire click event if not disabled', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<gux-button></gux-button>');
+    const element = await page.find('gux-button');
+    const onclick = await element.spyOnEvent('click');
+    await element.click();
+    expect(onclick).toHaveReceivedEventTimes(1);
+  });
+
+  it('Should not fire click event if disabled', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<gux-button></gux-button>');
+    const element = await page.find('gux-button');
+    const onclick = await element.spyOnEvent('click');
+    element.setAttribute('disabled', 'disabled');
+    await page.waitForChanges();
+    expect(element).toHaveAttribute('disabled');
+    expect(element).toEqualAttribute('disabled', '');
+    await element.click();
+    expect(onclick).toHaveReceivedEventTimes(0);
+  });
 });
