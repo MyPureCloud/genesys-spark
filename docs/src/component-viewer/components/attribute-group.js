13dc5dcfcc8230228353822bb097798cc5aff26f
Eric Lifka <eric.lifka@genesys.com>
2019-06-21 15:54:24 -0400

    docs: bring in new documentation system

---
+++ docs/src/component-viewer/components/attribute-group.js
@@ -0,0 +1,33 @@
+import { toHTML } from "../../utils/to-html";
+import COMPONENT_SPEC from "../../gux-components-spec.json";
+import { checkboxAttribute } from "./checkbox-attribute";
+import { selectAttribute } from "./select-attribute";
+import { textAttribute } from "./text-attribute";
+import { jsonAttribute } from "./json-attribute";
+
+const handlers = {
+    'checkbox': checkboxAttribute,
+    'select': selectAttribute,
+    'text': textAttribute,
+    'json': jsonAttribute
+};
+
+export const createAttributeGroup = (parent, astNode, renderCallback) => {
+    let type = astNode.nodeName;
+    let element = toHTML(`
+        <div class="attribute-group">
+            <div class="title">${type}</div>
+        </div>`);
+
+    parent.appendChild(element);
+
+    let attributes = COMPONENT_SPEC[ type ].attributes || [ ];
+    attributes.forEach(attr => {
+        let handler = handlers[ attr.type ];
+        if (handler) {
+            handler(attr, astNode, element, renderCallback);
+        } else {
+            console.error('Found attribute type with no implemented renderer: ', attr);
+        }
+    });
+};
---
+++ docs/src/component-viewer/components/text-attribute.js
@@ -0,0 +1,28 @@
+import { toHTML } from "../../utils/to-html";
+import { debounce } from "../../utils/debounce";
+
+export const textAttribute = (attr, astNode, parent, renderCallback) => {
+    let matchingAttr = astNode.attrs.find(a => a.name === attr.name);
+    let value = matchingAttr ? matchingAttr.value : "";
+
+    let element = toHTML(`
+    <label>
+        <span>${attr.name}:</span>
+        <input type="text" value="${value}" />
+    </label>`);
+
+    element.addEventListener('input', debounce(event => {
+        let value = event.target.value;
+        if (matchingAttr) {
+            matchingAttr.value = value;
+        } else if (value) {
+            astNode.attrs.push({ name: attr.name, value });
+        } else {
+            astNode.attrs = astNode.attrs.filter(a => a.name !== attr.name);
+        }
+
+        renderCallback();
+    }));
+
+    parent.appendChild(element);
+};
