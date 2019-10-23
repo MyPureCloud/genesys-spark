5f90948c57744094381af0c166f100e09cb71ca8
Matthew Cheely <matthew.cheely@genesys.com>
2019-10-23 11:22:56 -0400

    style: remove unused component category structure
    the global/rich/feature component directories aren't really being used so we might as well just
    remove them until we actually feel like we need to organize components

---
+++ src/components/gux-tooltip/gux-tooltip.tsx
@@ -0,0 +1,183 @@
+import {
+  Component,
+  Element,
+  Event,
+  EventEmitter,
+  h,
+  Listen,
+  Method,
+  Prop,
+  State
+} from '@stencil/core';
+import { getPositionRelativeToTarget } from '../../common-utils';
+
+@Component({
+  styleUrl: 'gux-tooltip.less',
+  tag: 'gux-tooltip'
+})
+export class GuxTooltip {
+  @Element()
+  root: HTMLGuxTooltipElement;
+
+  /**
+   * Element's id.
+   */
+  @Prop()
+  for: string;
+  /**
+   * Tooltip text.
+   */
+  @Prop()
+  text: string;
+  /**
+   * Delay before hide. (Set to 0 to none)
+   */
+  @Prop()
+  delay: number = 1000;
+  /**
+   * Tooltip current state.
+   */
+  @Prop({ mutable: true, reflectToAttr: true })
+  isShown: boolean = false;
+
+  @State()
+  tooltipRect: { [s: string]: number };
+
+  forNode: HTMLElement;
+  tooltipEl: HTMLElement;
+
+  positionOptions = {
+    offsetX: 0,
+    offsetY: 24,
+    width: undefined
+  };
+
+  initialWidth: number;
+
+  mouseenterHandler: () => void;
+  mouseleaveHandler: () => void;
+  scrollHandler: () => void;
+
+  delayTimeout: NodeJS.Timer;
+
+  /**
+   * Triggered when the tooltip is shown
+   */
+  @Event()
+  shown: EventEmitter;
+  /**
+   * Triggered when the tooltip is hidden
+   */
+  @Event()
+  hidden: EventEmitter;
+
+  emitEvent() {
+    if (this.isShown) {
+      this.shown.emit();
+    } else {
+      this.hidden.emit();
+    }
+  }
+
+  /**
+   * Shows the tooltip.
+   * @param duration Time before
+   */
+  @Method()
+  async show() {
+    this.delayTimeout = setTimeout(() => {
+      this.tooltipRect = getPositionRelativeToTarget(
+        this.tooltipEl,
+        this.forNode,
+        this.positionOptions
+      );
+      this.isShown = true;
+      this.emitEvent();
+    }, this.delay);
+  }
+
+  /**
+   * Hides the tooltip.
+   */
+  @Method()
+  async hide() {
+    clearTimeout(this.delayTimeout);
+    this.isShown = false;
+    this.emitEvent();
+  }
+
+  @Listen('resize', { capture: true })
+  @Listen('scroll', { capture: true })
+  onWindowEvent() {
+    this.tooltipRect = getPositionRelativeToTarget(
+      this.tooltipEl,
+      this.forNode,
+      this.positionOptions
+    );
+  }
+
+  get computedClass() {
+    return `gux-tooltip ${this.isShown ? 'shown' : ''}`;
+  }
+
+  get computedStyle() {
+    return {
+      bottom:
+        this.tooltipRect && this.tooltipRect.hasOwnProperty('bottom')
+          ? this.tooltipRect.bottom + 'px'
+          : '',
+      left:
+        this.tooltipRect && this.tooltipRect.hasOwnProperty('left')
+          ? this.tooltipRect.left + 'px'
+          : '',
+      right:
+        this.tooltipRect && this.tooltipRect.hasOwnProperty('right')
+          ? this.tooltipRect.right + 'px'
+          : '',
+      top:
+        this.tooltipRect && this.tooltipRect.hasOwnProperty('top')
+          ? this.tooltipRect.top + 'px'
+          : ''
+    };
+  }
+
+  componentDidLoad() {
+    this.forNode = document.getElementById(this.for) || this.root.parentElement;
+
+    this.tooltipRect = getPositionRelativeToTarget(
+      this.tooltipEl,
+      this.forNode,
+      this.positionOptions
+    );
+
+    this.mouseenterHandler = () => {
+      this.show();
+    };
+    this.mouseleaveHandler = () => {
+      this.hide();
+    };
+
+    this.forNode.addEventListener('mouseenter', this.mouseenterHandler);
+    this.forNode.addEventListener('mouseleave', this.mouseleaveHandler);
+
+    this.positionOptions.width =
+      this.tooltipEl.getBoundingClientRect().width + 6;
+  }
+
+  componentDidUnload() {
+    this.forNode.removeEventListener('mouseenter', this.mouseenterHandler);
+    this.forNode.removeEventListener('mouseleave', this.mouseleaveHandler);
+  }
+
+  render() {
+    return (
+      <div
+        class={this.computedClass}
+        ref={el => (this.tooltipEl = el)}
+        style={this.computedStyle}
+      >
+        {this.text}
+      </div>
+    );
+  }
+}
---
+++ src/components/gux-tooltip/tests/gux-tooltip.spec.ts
@@ -0,0 +1,20 @@
+import * as Utils from '../../../common-utils';
+import { GuxTooltip } from '../gux-tooltip';
+
+describe('gux-tooltip', () => {
+  let component;
+  beforeEach(async () => {
+    component = new GuxTooltip();
+  });
+  it('builds', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('methods', () => {
+    it('resize', () => {
+      spyOn(Utils, 'getPositionRelativeToTarget');
+      component.onWindowEvent();
+      expect(Utils.getPositionRelativeToTarget).toHaveBeenCalled();
+    });
+  });
+});
