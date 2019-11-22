492c244b92825fe303f01006141f44ec99751631
Josiah Campbell <josiah.campbell@inin.com>
2019-11-22 16:17:47 -0500

    feat(gux-icon): address pr commnets
    change argument names for gux-icon. remove unneeded readme and stop stencil from autogenerating
    them.

--- src/components/gux-icon/gux-icon.tsx
+++ src/components/gux-icon/gux-icon.tsx
@@ -9,7 +9,7 @@ export class GuxIcon {
    * Indicate which icon to display
    */
   @Prop()
-  iconname: string;
+  iconName: string;
 
   /**
    * Indicate whether the icon should be ignored by accessibility tools or not
@@ -21,31 +21,20 @@ export class GuxIcon {
    * Localized text describing the intent of this icon (not required if `decorative=true`)
    */
   @Prop()
-  label: string;
-
-  /**
-   * Sets the height and width of the icon (in pixels)
-   */
-  @Prop()
-  size: number;
+  screenreaderText: string;
 
   componentDidLoad() {
-    if (!this.decorative && !this.label) {
+    if (!this.decorative && !this.screenreaderText) {
       console.error(
-        '[gux-icon] No arialabel provided. Either provide a localized arialabel property or set `decorative` to true'
+        '[gux-icon] No screenreader-text provided. Either provide a localized screenreader-text property or set `decorative` to true'
       );
     }
   }
 
   render() {
     return (
-      <svg
-        aria-hidden={this.decorative}
-        aria-label={this.label}
-        height={this.size}
-        width={this.size}
-      >
-        <use xlinkHref={`svg-icons/genesys-icons.svg#${this.iconname}`} />
+      <svg aria-hidden={this.decorative} aria-label={this.screenreaderText}>
+        <use xlinkHref={`svg-icons/genesys-icons.svg#${this.iconName}`} />
       </svg>
     );
   }
