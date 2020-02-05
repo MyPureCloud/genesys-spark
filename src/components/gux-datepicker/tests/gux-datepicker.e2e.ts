0f5014aabe634f0d70164f1b4e5a3cd4880b32e7
Kevin Grey <kevin.grey@genesys.com>
2020-02-05 21:11:43 +0000

    test(gux-datepicker): Test for DOM updating
    Test to check value propegates to DOM

--- src/components/gux-datepicker/tests/gux-datepicker.e2e.ts
+++ src/components/gux-datepicker/tests/gux-datepicker.e2e.ts
@@ -8,4 +8,17 @@ describe('gux-datepicker', () => {
     const element = await page.find('gux-datepicker');
     expect(element).toHaveClass('hydrated');
   });
+  it('updates the DOM when the value changes', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<gux-datepicker></gux-datepicker>');
+    const element = await page.find('gux-datepicker');
+
+    await element.setProperty('value', '1985-12-01');
+    await page.waitForChanges();
+
+    const input = await page.find('gux-text-field input');
+    const value = await input.getProperty('value');
+    expect(value).toBe('12/01/1985');
+  });
 });
