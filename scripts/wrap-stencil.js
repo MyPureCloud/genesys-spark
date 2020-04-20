f873ff3e4951afd78ac4a01e42b37c7199e3bc53
Thiyagarajan Ambalavanan <thiyagarajan.ambalavanan@genesys.com>
2020-04-21 00:24:54 +0530

    fix(type-generation): automatic type generation

--- scripts/wrap-stencil.js
+++ scripts/wrap-stencil.js
@@ -3,7 +3,6 @@
 const Handlebars = require('handlebars');
 const path = require('path');
 const fs = require('fs');
-const proc = require('child_process');
 
 const CDN_URL = process.env.CDN_URL || '';
 console.log(`Wrapping stencil loader.`);
@@ -13,12 +12,9 @@ if (CDN_URL.length > 0) {
   console.log(`  No CDN URL specified.`);
 }
 
-const tscPath = require.resolve('.bin/tsc');
 const templateFile = path.join(__dirname, './stencil-wrapper.ts');
 const sourceOutputFolder = path.join(__dirname, '../build');
 const sourceOutputFile = path.join(sourceOutputFolder, 'stencil-wrapper.ts');
-const distDir = path.join(__dirname, '../dist');
-const typesDir = path.join(distDir, 'types');
 const template = Handlebars.compile(fs.readFileSync(templateFile, 'utf8'));
 
 const source = template({ cdn_url: CDN_URL });
@@ -27,7 +23,3 @@ if (!fs.existsSync(sourceOutputFolder)) {
   fs.mkdirSync(sourceOutputFolder, { recursive: true });
 }
 fs.writeFileSync(sourceOutputFile, source);
-
-proc.execSync(
-  `${tscPath} --declaration --declarationDir ${typesDir} --outDir ${distDir}  ${sourceOutputFile}`
-);
--- src/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './components';
-
-// This must be kept in sync with `scripts/stencil-wrapper.ts` until we
-// find a way use the types form that file directly.
-export declare function registerElements(): void;
