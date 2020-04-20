34340f652dd581a1541d6466091b85863ed00a10
Matthew Cheely <matthew.cheely@genesys.com>
2020-04-20 12:36:42 -0400

    build(stencil-wrapper): generate types for stencil-wrapper.ts
    We can't export them directly yet, but this updates the stencil-wrapper.ts compliation to generate
    type definitions. It also add some clarifying commeents to index.d.ts

--- scripts/wrap-stencil.js
+++ scripts/wrap-stencil.js
@@ -6,13 +6,19 @@ const fs = require('fs');
 const proc = require('child_process');
 
 const CDN_URL = process.env.CDN_URL || '';
-console.log(`Wrapping stencil loader. CDN URL will be: ${CDN_URL}`);
+console.log(`Wrapping stencil loader.`);
+if (CDN_URL.length > 0) {
+  console.log(`  Embedded CDN URL will be: ${CDN_URL}`);
+} else {
+  console.log(`  No CDN URL specified.`);
+}
 
 const tscPath = require.resolve('.bin/tsc');
 const templateFile = path.join(__dirname, './stencil-wrapper.ts');
 const sourceOutputFolder = path.join(__dirname, '../build');
 const sourceOutputFile = path.join(sourceOutputFolder, 'stencil-wrapper.ts');
 const distDir = path.join(__dirname, '../dist');
+const typesDir = path.join(distDir, 'types');
 const template = Handlebars.compile(fs.readFileSync(templateFile, 'utf8'));
 
 const source = template({ cdn_url: CDN_URL });
@@ -22,4 +28,6 @@ if (!fs.existsSync(sourceOutputFolder)) {
 }
 fs.writeFileSync(sourceOutputFile, source);
 
-proc.execSync(`${tscPath} ${sourceOutputFile} --outDir ${distDir}`);
+proc.execSync(
+  `${tscPath} --declaration --declarationDir ${typesDir} --outDir ${distDir}  ${sourceOutputFile}`
+);
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,5 @@
 export * from './components';
 
+// This must be kept in sync with `scripts/stencil-wrapper.ts` until we
+// find a way use the types form that file directly.
 export declare function registerElements(): void;
