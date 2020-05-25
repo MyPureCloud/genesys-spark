81a919a5c03ed4a916348bd4565d9cd9f33160a7
Daragh King <daragh.king@genesys.com>
2020-05-25 12:31:28 +0100

    improvement(gux-page-loading-spinner): centers horizontally by default
    BREAKING CHANGE: There is no need for client apps to centre this component manually anymore
    
    COMUI-144

--- src/components/gux-page-loading-spinner/tests/gux-page-loading-spinner.spec.ts
+++ src/components/gux-page-loading-spinner/tests/gux-page-loading-spinner.spec.ts
@@ -1,20 +1,14 @@
-import { newSpecPage } from '@stencil/core/testing';
+import { newSpecPage, SpecPage } from '@stencil/core/testing';
 import { GuxPageLoadingSpinner } from '../gux-page-loading-spinner';
 
 describe('gux-page-loading-spinner', () => {
-  let component: GuxPageLoadingSpinner;
-
-  beforeEach(async () => {
+  it('should render as expected', async () => {
     const page = await newSpecPage({
       components: [GuxPageLoadingSpinner],
       html: `<gux-page-loading-spinner></gux-page-loading-spinner>`,
       language: 'en'
     });
 
-    component = page.rootInstance;
-  });
-
-  it('should build', async () => {
-    expect(component).toBeInstanceOf(GuxPageLoadingSpinner);
+    expect(page.root).toMatchSnapshot();
   });
 });
