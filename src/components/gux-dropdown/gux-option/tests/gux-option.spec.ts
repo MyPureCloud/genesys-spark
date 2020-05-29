1e7969b41820ef6e52bbd45e69aff7b03d1d9f43
Manoj Babu Porur Ragu <manojbabu.ragu@genesys.com>
2020-05-29 18:20:22 +0530

    improvement(gux-dropdown): Removing JS array in Gux-Dropdown
    Removing Js array and adding Gux-option as child element
    
    BREAKING CHANGE: Ilist has been removed from gux-dropdown

---
+++ src/components/gux-dropdown/gux-option/tests/gux-option.spec.ts
@@ -0,0 +1,58 @@
+import { newSpecPage } from '@stencil/core/testing';
+import { GuxOption } from '../gux-option';
+
+describe('gux-option', () => {
+  let component: GuxOption;
+
+  beforeEach(async () => {
+    const page = await newSpecPage({
+      components: [GuxOption],
+      html: `<gux-option></gux-option>`,
+      language: 'en'
+    });
+
+    component = page.rootInstance;
+  });
+
+  it('should build', async () => {
+    expect(component).toBeInstanceOf(GuxOption);
+  });
+
+  describe('Class Logic', () => {
+    describe('shouldFilter', () => {
+      it('should not filter if no search string is provided', async () => {
+        const result = await component.shouldFilter('');
+
+        expect(result).toBeFalsy();
+      });
+
+      it('should not filter if string is in the text', async () => {
+        component.text = 'TestsAreAwesome';
+
+        const result = await component.shouldFilter('Are');
+
+        expect(result).toBeFalsy();
+        expect(component.highlight).toBe('Are');
+        expect(component.highlightIndex).toBe(5);
+      });
+
+      it('should not filter if case-insensitive string is in the text', async () => {
+        component.text = 'TestsAreAwesome';
+
+        const result = await component.shouldFilter('are');
+
+        expect(result).toBeFalsy();
+        expect(component.highlight).toBe('are');
+        expect(component.highlightIndex).toBe(5);
+      });
+
+      it('should filter if string is not in the text', async () => {
+        component.text = 'TestsAreAwesome';
+
+        const result = await component.shouldFilter('Not');
+
+        expect(result).toBeTruthy();
+      });
+    });
+  });
+});
