9827a5a0f2901210b0360704f686c82c4c0155f9
Matthew Cheely <matthew.cheely@genesys.com>
2020-06-09 12:57:06 -0400

    ci(readme-check): only check for readme changes
    The CI build was picking up package-lock.json changes as required
    readme updates.

--- scripts/check-readmes.sh
+++ scripts/check-readmes.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [[ $(git status -s) ]]; then
+README_UPDATES=$(git status -s | grep -i "src/components/.*/readme.md")
+
+if [[ "${README_UPDATES}" ]]; then
   echo -e "\033[0;31m\nThere were component readme updates made by the build process that are not present in this version of the project.\033[0m"
-  git status -s
+  echo -e "${README_UPDATES}"
   echo -e "\033[0;31mPlease built the project locally and make sure all relevent component readme changes are committed.\n\033[0m"
   exit 1
 fi
