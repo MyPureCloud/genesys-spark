69969554dff09545b7effd7d712bbadc73065d1e
Klimonov <22pov22@gmail.com>
2020-08-25 14:37:09 +0300

    fix: returned the remote code

--- src/components/stable/gux-action-button/tests/gux-action-button.e2e.ts
+++ src/components/stable/gux-action-button/tests/gux-action-button.e2e.ts
@@ -8,4 +8,49 @@ describe('gux-action-button', () => {
     const element = await page.find('gux-action-button');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('Should fire actionClick event if not disabled', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<gux-action-button></gux-action-button>');
+    const element = await page.find('gux-action-button');
+    const onActionClick = await element.spyOnEvent('actionClick');
+    await element.click();
+    expect(onActionClick).toHaveReceivedEventTimes(1);
+  });
+
+  it('Should not fire actionClick event if disabled', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<gux-action-button></gux-action-button>');
+    const element = await page.find('gux-action-button');
+    const onActionClick = await element.spyOnEvent('actionClick');
+    element.setAttribute('disabled', 'disabled');
+    await page.waitForChanges();
+    expect(element).toHaveAttribute('disabled');
+    expect(element).toEqualAttribute('disabled', 'disabled');
+    await element.click();
+    expect(onActionClick).toHaveReceivedEventTimes(0);
+  });
+
+  it('Should fire open event if not disabled', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<gux-action-button></gux-action-button>');
+    const element = await page.find('gux-action-button');
+    const dropdownElm = await element.find('.gux-dropdown');
+    const onOpen = await element.spyOnEvent('open');
+    await dropdownElm.click();
+    expect(onOpen).toHaveReceivedEventTimes(1);
+  });
+
+  it('Should not fire open event if disabled', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<gux-action-button></gux-action-button>');
+    const element = await page.find('gux-action-button');
+    const onOpen = await element.spyOnEvent('open');
+    element.setAttribute('disabled', 'disabled');
+    await page.waitForChanges();
+    expect(element).toHaveAttribute('disabled');
+    expect(element).toEqualAttribute('disabled', 'disabled');
+    await element.click();
+    expect(onOpen).toHaveReceivedEventTimes(0);
+  });
 });
