fd7f6dedb5d23b9cd968002f337a78aa8412b087
Matthew Cheely <matthew.cheely@genesys.com>
2020-09-25 15:22:25 -0400

    docs(attribute-panel): hide attribute panel with gux-disclosure-button
    This hides the attribute/event panel in the demo docs with a gux-disclosure-button. It's not the
    most discoverable approach, but it's a good quick fix to make the embedded docs look a lot less
    cluttered, IMO.
    
    COMUI-270

--- docs/src/component-viewer/app.js
+++ docs/src/component-viewer/app.js
@@ -24,16 +24,18 @@ function createLayout() {
         <div class="preview gux-light-theme"></div>
         <div class="editor"></div>
       </div>
-      <div class="right-column">
-        <details>
-          <summary class="heading">Attributes</summary>
-          <div class="attributes"></div>
-        </details>
-        <details>
-          <summary class="heading">Event Details</summary>
-          <div class="events"></div>
-        </details>
-      </div>
+      <gux-disclosure-button position="right">
+        <div slot="panel-content" class="controls-column">
+          <details>
+            <summary class="heading">Event Descriptions</summary>
+            <div class="events"></div>
+          </details>
+          <details>
+            <summary class="heading">Attributes</summary>
+            <div class="attributes"></div>
+          </details>
+        </div>
+      </gux-disclosure-button>
       <div class="notification"></div>
     </div>
   `);
