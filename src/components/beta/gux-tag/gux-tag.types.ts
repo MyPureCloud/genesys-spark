9b0fccb464dd5e13334ed5a7291556dec87e9ec4
Daragh King <daragh.king@genesys.com>
2020-10-09 11:08:12 +0100

    build(stencil): only export component class from component file
    only export component class from component file
    
    COMUI-286

---
+++ src/components/beta/gux-tag/gux-tag.types.ts
@@ -0,0 +1,12 @@
+export type GuxTagColor =
+  | 'default'
+  | 'navy'
+  | 'blue'
+  | 'electric-purple'
+  | 'aqua-green'
+  | 'fuscha'
+  | 'dark-purple'
+  | 'bubblegum-pink'
+  | 'olive-green'
+  | 'lilac'
+  | 'yellow-green';
