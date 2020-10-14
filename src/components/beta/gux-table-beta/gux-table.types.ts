shreya-chauhan/genesys-webcomponents

feat(gux-table): row selection implementation …
row selection implementation …

src/components/beta/gux-table-beta/gux-table.types.ts
@@ -8,3 +8,8 @@ export interface ISortState {
   columnName: string;
   sortDirection: string;
 }
+
+export interface ISelectedState {
+  rowIds: string[];
+  actionType: 'selected' | 'unselected';
+}

MyPureCloud/genesys-spark

src/components/beta/gux-table-beta/gux-table.types.ts
@@ -1,10 +1,14 @@
-export interface IColumnResizeState {
+export interface GuxTableColumnResizeState {
   resizableColumn: HTMLElement;
   resizableColumnInitialWidth: number;
   columnResizeMouseStartX: number;
 }
 
-export interface ISortState {
+export interface GuxTableSortState {
   columnName: string;
   sortDirection: string;
+}
+
+export interface GuxTableSelectedState {
+  selectedRowIds: string[];
 }