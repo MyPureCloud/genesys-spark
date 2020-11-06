3eb4a08d3aa79913066b40456d91783c8bad889c
Daragh King <daragh.king@genesys.com>
2020-11-06 11:42:28 +0000

    feat(gux-rating): updated public api
    Property and Event Changes
      - Properties
        - `allowHalfRatings` has been removed.
        - `labels` has been removed.
        - `svgPoints` has been removed.
        - `svgViewBox` has been removed.
        - `disabled` was not changed.
        - `readonly` was added.
        - `rating` has been renamed value.
        - `maxRating` has been renamed maxValue.
    
      - Events
        The events are now native `change` and `input` events to match the new `value` property
        - `update` has been removed.
        - native like `input` and `change` events are not emitted
    
    COMUI-150

---
+++ src/components/beta/gux-rating/gux-rating-star/readme.md
@@ -0,0 +1,31 @@
+# gux-rating
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property   | Attribute  | Description | Type                          | Default   |
+| ---------- | ---------- | ----------- | ----------------------------- | --------- |
+| `disabled` | `disabled` |             | `boolean`                     | `false`   |
+| `fill`     | `fill`     |             | `"empty" \| "full" \| "half"` | `'empty'` |
+
+
+## Dependencies
+
+### Used by
+
+ - [gux-rating-beta](..)
+
+### Graph
+```mermaid
+graph TD;
+  gux-rating-beta --> gux-rating-star
+  style gux-rating-star fill:#f9f,stroke:#333,stroke-width:4px
+```
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
--- src/components/beta/gux-rating/tests/gux-rating.spec.ts
+++ src/components/beta/gux-rating/tests/gux-rating.spec.ts
@@ -1,48 +1,46 @@
+jest.mock('../../../../utils/dom/random-html-id', () => ({
+  randomHTMLId: () => 'random'
+}));
+
 import { newSpecPage, SpecPage } from '@stencil/core/testing';
 import { GuxRating } from '../gux-rating';
+import { GuxRatingStar } from '../gux-rating-star/gux-rating-star';
+
+global.InputEvent = Event;
+
+const components = [GuxRating, GuxRatingStar];
+const language = 'en';
 
 describe('gux-rating', () => {
-  let component: GuxRating;
-
-  beforeEach(async () => {
-    const page = await newSpecPage({
-      components: [GuxRating],
-      html: `<gux-rating-beta></gux-rating-beta>`,
-      language: 'en'
-    });
-
-    component = page.rootInstance;
-  });
-
   it('should build', async () => {
-    expect(component).toBeInstanceOf(GuxRating);
+    const html = `<gux-rating-beta></gux-rating-beta>`;
+    const page = await newSpecPage({ components, html, language });
+
+    expect(page.rootInstance).toBeInstanceOf(GuxRating);
   });
 
   describe('#render', () => {
     [
       '<gux-rating-beta></gux-rating-beta>',
-      '<gux-rating-beta rating="0"></gux-rating-beta>',
-      '<gux-rating-beta rating="1"></gux-rating-beta>',
-      '<gux-rating-beta rating="2"></gux-rating-beta>',
-      '<gux-rating-beta rating="3"></gux-rating-beta>',
-      '<gux-rating-beta rating="4"></gux-rating-beta>',
-      '<gux-rating-beta rating="5"></gux-rating-beta>',
-      '<gux-rating-beta allow-half-ratings rating="0.5"></gux-rating-beta>',
-      '<gux-rating-beta allow-half-ratings rating="1.5"></gux-rating-beta>',
-      '<gux-rating-beta allow-half-ratings rating="2.5"></gux-rating-beta>',
-      '<gux-rating-beta allow-half-ratings rating="3.5"></gux-rating-beta>',
-      '<gux-rating-beta allow-half-ratings rating="4.5"></gux-rating-beta>',
-      '<gux-rating-beta rating="0" max-rating=10></gux-rating-beta>',
-      '<gux-rating-beta rating="5" max-rating=10></gux-rating-beta>',
-      '<gux-rating-beta rating="10" max-rating=10></gux-rating-beta>',
-      '<gux-rating-beta rating="3" disabled></gux-rating-beta>'
-    ].forEach((input, index) => {
+      '<gux-rating-beta value="0"></gux-rating-beta>',
+      '<gux-rating-beta value="0.5"></gux-rating-beta>',
+      '<gux-rating-beta value="1"></gux-rating-beta>',
+      '<gux-rating-beta value="1.5"></gux-rating-beta>',
+      '<gux-rating-beta value="2"></gux-rating-beta>',
+      '<gux-rating-beta value="2.5"></gux-rating-beta>',
+      '<gux-rating-beta value="3"></gux-rating-beta>',
+      '<gux-rating-beta value="3.5"></gux-rating-beta>',
+      '<gux-rating-beta value="4"></gux-rating-beta>',
+      '<gux-rating-beta value="4.5"></gux-rating-beta>',
+      '<gux-rating-beta value="5"></gux-rating-beta>',
+      '<gux-rating-beta value="0" max-value=10></gux-rating-beta>',
+      '<gux-rating-beta value="5" max-value=10></gux-rating-beta>',
+      '<gux-rating-beta value="10" max-value=10></gux-rating-beta>',
+      '<gux-rating-beta value="3" disabled></gux-rating-beta>',
+      '<gux-rating-beta value="3" readonly></gux-rating-beta>'
+    ].forEach((html, index) => {
       it(`should render component as expected (${index + 1})`, async () => {
-        const page = await newSpecPage({
-          components: [GuxRating],
-          html: input,
-          language: 'en'
-        });
+        const page = await newSpecPage({ components, html, language });
 
         expect(page.root).toMatchSnapshot();
       });
@@ -51,10 +49,10 @@ describe('gux-rating', () => {
 
   describe('#interactions', () => {
     async function clickStar(page: SpecPage, position: number): Promise<void> {
-      const ratingElements = page.doc.getElementsByClassName(
-        'gux-rating-element'
+      const ratingStarElements = page.doc.getElementsByTagName(
+        'gux-rating-star'
       );
-      const ratingElement = ratingElements[position - 1] as HTMLElement;
+      const ratingElement = ratingStarElements[position - 1] as HTMLElement;
 
       ratingElement.click();
 
@@ -66,23 +64,23 @@ describe('gux-rating', () => {
     ): { emptyStars: number; halfStars: number; fullStars: number } {
       return {
         emptyStars: document.getElementsByClassName(
-          'gux-rating-item-shape-fill-percent-0'
-        ).length,
-        fullStars: document.getElementsByClassName(
-          'gux-rating-item-shape-fill-percent-100'
+          'gux-rating-star-fill-percent-0'
         ).length,
         halfStars: document.getElementsByClassName(
-          'gux-rating-item-shape-fill-percent-50'
+          'gux-rating-star-fill-percent-50'
+        ).length,
+        fullStars: document.getElementsByClassName(
+          'gux-rating-star-fill-percent-100'
         ).length
       };
     }
 
-    describe('full ratings', () => {
+    describe('ratings', () => {
       let page: SpecPage;
 
       beforeEach(async () => {
         page = await newSpecPage({
-          components: [GuxRating],
+          components,
           html: '<gux-rating-beta></gux-rating-beta>'
         });
       });
@@ -95,68 +93,7 @@ describe('gux-rating', () => {
         });
       });
 
-      it(`should render one full stars if the first one is clicked`, async () => {
-        await clickStar(page, 1);
-
-        expect(getStarCounts(page.doc)).toEqual({
-          emptyStars: 4,
-          fullStars: 1,
-          halfStars: 0
-        });
-      });
-
-      it(`should render five empty stars if the first one is clicked twice`, async () => {
-        await clickStar(page, 1);
-        await clickStar(page, 1);
-
-        expect(getStarCounts(page.doc)).toEqual({
-          emptyStars: 5,
-          fullStars: 0,
-          halfStars: 0
-        });
-      });
-
-      it(`should render five full stars if the fifth one is clicked`, async () => {
-        await clickStar(page, 5);
-
-        expect(getStarCounts(page.doc)).toEqual({
-          emptyStars: 0,
-          fullStars: 5,
-          halfStars: 0
-        });
-      });
-
-      it(`should render five empty stars if the fifth one is clicked twice`, async () => {
-        await clickStar(page, 5);
-        await clickStar(page, 5);
-
-        expect(getStarCounts(page.doc)).toEqual({
-          emptyStars: 5,
-          fullStars: 0,
-          halfStars: 0
-        });
-      });
-    });
-
-    describe('half ratings', () => {
-      let page: SpecPage;
-
-      beforeEach(async () => {
-        page = await newSpecPage({
-          components: [GuxRating],
-          html: '<gux-rating-beta allow-half-ratings></gux-rating-beta>'
-        });
-      });
-
-      it(`should render five empty stars if nothing is clicked`, async () => {
-        expect(getStarCounts(page.doc)).toEqual({
-          emptyStars: 5,
-          fullStars: 0,
-          halfStars: 0
-        });
-      });
-
-      it(`should render one half star if the first one is clicked`, async () => {
+      it(`should render 0.5 if the first one is clicked`, async () => {
         await clickStar(page, 1);
 
         expect(getStarCounts(page.doc)).toEqual({
@@ -166,7 +103,7 @@ describe('gux-rating', () => {
         });
       });
 
-      it(`should render one full star if the first one is clicked twice`, async () => {
+      it(`should render 1.0 if the first one is clicked twice`, async () => {
         await clickStar(page, 1);
         await clickStar(page, 1);
 
@@ -177,7 +114,7 @@ describe('gux-rating', () => {
         });
       });
 
-      it(`should render no full stars if the first one is clicked three times`, async () => {
+      it(`should render 0.0 if the first one is clicked three times`, async () => {
         await clickStar(page, 1);
         await clickStar(page, 1);
         await clickStar(page, 1);
@@ -189,7 +126,106 @@ describe('gux-rating', () => {
         });
       });
 
-      it(`should render four full stars and one half one if the fifth one is clicked`, async () => {
+      it(`should render 1.5 if the second one is clicked`, async () => {
+        await clickStar(page, 2);
+
+        expect(getStarCounts(page.doc)).toEqual({
+          emptyStars: 3,
+          fullStars: 1,
+          halfStars: 1
+        });
+      });
+
+      it(`should render 2.0 if the second one is clicked twice`, async () => {
+        await clickStar(page, 2);
+        await clickStar(page, 2);
+
+        expect(getStarCounts(page.doc)).toEqual({
+          emptyStars: 3,
+          fullStars: 2,
+          halfStars: 0
+        });
+      });
+
+      it(`should render 0.0 if the second one is clicked three times`, async () => {
+        await clickStar(page, 2);
+        await clickStar(page, 2);
+        await clickStar(page, 2);
+
+        expect(getStarCounts(page.doc)).toEqual({
+          emptyStars: 5,
+          fullStars: 0,
+          halfStars: 0
+        });
+      });
+
+      it(`should render 2.5 if the third one is clicked`, async () => {
+        await clickStar(page, 3);
+
+        expect(getStarCounts(page.doc)).toEqual({
+          emptyStars: 2,
+          fullStars: 2,
+          halfStars: 1
+        });
+      });
+
+      it(`should render 3.0 if the third one is clicked twice`, async () => {
+        await clickStar(page, 3);
+        await clickStar(page, 3);
+
+        expect(getStarCounts(page.doc)).toEqual({
+          emptyStars: 2,
+          fullStars: 3,
+          halfStars: 0
+        });
+      });
+
+      it(`should render 0.0 if the third one is clicked three times`, async () => {
+        await clickStar(page, 3);
+        await clickStar(page, 3);
+        await clickStar(page, 3);
+
+        expect(getStarCounts(page.doc)).toEqual({
+          emptyStars: 5,
+          fullStars: 0,
+          halfStars: 0
+        });
+      });
+
+      it(`should render 3.5 if the third one is clicked`, async () => {
+        await clickStar(page, 4);
+
+        expect(getStarCounts(page.doc)).toEqual({
+          emptyStars: 1,
+          fullStars: 3,
+          halfStars: 1
+        });
+      });
+
+      it(`should render 4.0 if the third one is clicked twice`, async () => {
+        await clickStar(page, 4);
+        await clickStar(page, 4);
+
+        expect(getStarCounts(page.doc)).toEqual({
+          emptyStars: 1,
+          fullStars: 4,
+          halfStars: 0
+        });
+      });
+
+      it(`should render 0.0 if the third one is clicked three times`, async () => {
+        await clickStar(page, 4);
+        await clickStar(page, 4);
+        await clickStar(page, 4);
+
+        expect(getStarCounts(page.doc)).toEqual({
+          emptyStars: 5,
+          fullStars: 0,
+          halfStars: 0
+        });
+      });
+
+      it(`should render 4.5 if the third one is clicked`, async () => {
         await clickStar(page, 5);
 
         expect(getStarCounts(page.doc)).toEqual({
@@ -199,7 +235,7 @@ describe('gux-rating', () => {
         });
       });
 
-      it(`should render five full stars if the fifth one is clicked twice`, async () => {
+      it(`should render 5.0 if the third one is clicked twice`, async () => {
         await clickStar(page, 5);
         await clickStar(page, 5);
 
@@ -210,7 +246,7 @@ describe('gux-rating', () => {
         });
       });
 
-      it(`should render no full stars if the fifth one is clicked three times`, async () => {
+      it(`should render 0.0 if the third one is clicked three times`, async () => {
         await clickStar(page, 5);
         await clickStar(page, 5);
         await clickStar(page, 5);
@@ -228,8 +264,41 @@ describe('gux-rating', () => {
 
       beforeEach(async () => {
         page = await newSpecPage({
-          components: [GuxRating],
-          html: '<gux-rating-beta rating="3" disabled></gux-rating-beta>'
+          components,
+          html: '<gux-rating-beta value="3" disabled></gux-rating-beta>'
+        });
+      });
+
+      it(`should render three full stars if nothing is clicked`, async () => {
+        expect(getStarCounts(page.doc)).toEqual({
+          emptyStars: 2,
+          fullStars: 3,
+          halfStars: 0
+        });
+      });
+
+      it(`should render three full stars if anything is clicked`, async () => {
+        await clickStar(page, 1);
+        await clickStar(page, 2);
+        await clickStar(page, 3);
+        await clickStar(page, 4);
+        await clickStar(page, 5);
+
+        expect(getStarCounts(page.doc)).toEqual({
+          emptyStars: 2,
+          fullStars: 3,
+          halfStars: 0
+        });
+      });
+    });
+
+    describe('readonly', () => {
+      let page: SpecPage;
+
+      beforeEach(async () => {
+        page = await newSpecPage({
+          components,
+          html: '<gux-rating-beta value="3" readonly></gux-rating-beta>'
         });
       });
 
