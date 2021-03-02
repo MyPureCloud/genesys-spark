0b80f4180fd95ab5808c1bbbc3931050f4f1399a
Daragh King <daragh.king@genesys.com>
2021-03-02 11:32:00 +0000

    fix(gux-popover): fixed class name missing gux prefix
    fixed class name missing gux prefix
    
    COMUI-425
    
    (cherry picked from commit 5242ea6b49111f7c9e20c397f8df355c9a9eb992)

--- src/components/stable/gux-popover/gux-popover.tsx
+++ src/components/stable/gux-popover/gux-popover.tsx
@@ -164,7 +164,7 @@ export class GuxPopover {
       <div class="gux-popover-wrapper">
         <div class="gux-arrow" data-popper-arrow />
         {this.displayDismissButton && (
-          <div class="title-bar">
+          <div class="gux-title-bar">
             <gux-icon
               class="gux-dismiss"
               icon-name="close"
