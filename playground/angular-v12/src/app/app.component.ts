a9ade06f352b4d80ecf01dc011aee8d160ce23a7
Daragh King <daragh.king@genesys.com>
2021-07-02 10:36:52 +0100

    feat(playground): added angular 12 playground

---
+++ playground/angular-v12/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+  // styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  hasError = true;
+
+  toggleHasError(): void {
+    this.hasError = !this.hasError;
+  }
+}
