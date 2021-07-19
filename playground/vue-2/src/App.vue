fb82307bd834adabaa198832dbca2be4c5a25ad7
Daragh King <daragh.king@genesys.com>
2021-07-19 14:33:14 +0100

    chore(playground): modify vue-2 playground to use components
    modify vue-2 playground to use components
    
    COMUI_565

---
+++ playground/vue-2/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" lang="en">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+import { registerElements } from '@genesys/common-webcomponents';
+registerElements();
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
