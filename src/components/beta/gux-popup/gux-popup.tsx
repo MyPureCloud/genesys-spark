5aef8e4d412fb72722b44309736268de5f03898d
Daragh King <daragh.king@genesys.com>
2021-07-20 15:26:57 +0100

    fix(action-button): fixed styling
    fixed styling
    
    COMUI-567

--- src/components/beta/gux-popup/gux-popup.tsx
+++ src/components/beta/gux-popup/gux-popup.tsx
@@ -29,7 +29,7 @@ export class GuxPopup {
           {
             name: 'offset',
             options: {
-              offset: [0, 1]
+              offset: [-1, 2]
             }
           },
           {
