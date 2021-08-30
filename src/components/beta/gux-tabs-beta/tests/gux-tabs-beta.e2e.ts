293fde18c0d9d0c36d8f2e099a973159ca4101fc
Katie Bobbe <katie.bobbe@genesys.com>
2021-08-30 16:32:20 -0400

    test(gux-tabs-beta): add tests to component

--- src/components/beta/gux-tabs-beta/tests/gux-tabs-beta.e2e.ts
+++ src/components/beta/gux-tabs-beta/tests/gux-tabs-beta.e2e.ts
@@ -17,8 +17,7 @@ async function mockRandomForE2ESnapshot({
 }
 
 describe('gux-tabs-beta', () => {
-  it('renders', async () => {
-    const html = `
+  const component = `
     <gux-tabs-beta lang="en">
       <gux-tab-list-beta slot="tab-list">
           <gux-tab-beta tab-id="2-1"><span>Tab Header 1</span></gux-tab-beta>
@@ -37,10 +36,59 @@ describe('gux-tabs-beta', () => {
       <gux-tab-panel-beta tab-id="2-4">Tab content 4</gux-tab-panel-beta>
       <gux-tab-panel-beta tab-id="2-5">Tab content 5</gux-tab-panel-beta>
     </gux-tabs-beta>
-    `;
-    const page = await mockRandomForE2ESnapshot({ html });
-    const element = await page.find('gux-tabs-beta');
+`;
+  describe('#render', () => {
+    it('renders', async () => {
+      const html = component;
+      const page = await mockRandomForE2ESnapshot({ html });
+      const element = await page.find('gux-tabs-beta');
 
-    expect(element.outerHTML).toMatchSnapshot();
+      expect(element.outerHTML).toMatchSnapshot();
+    });
+
+    it('does not render the scroll buttons when tabs fit container', async () => {
+      const html = component;
+      const page = await mockRandomForE2ESnapshot({ html });
+      const scrollButtons = await page.findAll('.gux-scroll-button');
+      expect(scrollButtons.length).toBe(0);
+    });
+
+    it('renders scroll buttons when tabs overflow container', async () => {
+      const html = `<div style="width: 500px">${component}</div>`;
+      const page = await mockRandomForE2ESnapshot({ html });
+      const scrollButtons = await page.findAll('.gux-scroll-button');
+      expect(scrollButtons.length).toBe(2);
+    });
+  });
+
+  describe('#interactions', () => {
+    it('should change tabpanel content when tab is changed', async () => {
+      const html = component;
+      const page = await mockRandomForE2ESnapshot({ html });
+      const tabTarget = await page.find('gux-tab-beta[tab-id="2-2"]');
+      const tabPanel = await page.find('gux-tab-panel-beta[tab-id="2-2"]');
+      const spyOnActivePanelChangeEvent = await tabPanel.spyOnEvent(
+        'guxactivepanelchange'
+      );
+
+      tabTarget.click();
+      await page.waitForChanges();
+
+      expect(spyOnActivePanelChangeEvent.length).toBe(1);
+    });
+    it('should not change tabpanel content when tab is disabled', async () => {
+      const html = component;
+      const page = await mockRandomForE2ESnapshot({ html });
+      const tabTarget = await page.find('gux-tab-beta[tab-id="2-4"]');
+      const tabPanel = await page.find('gux-tab-panel-beta[tab-id="2-4"]');
+      const spyOnActivePanelChangeEvent = await tabPanel.spyOnEvent(
+        'guxactivepanelchange'
+      );
+
+      tabTarget.click();
+      await page.waitForChanges();
+
+      expect(spyOnActivePanelChangeEvent.length).toBe(0);
+    });
   });
 });
