cf860a3853a0ad23fac7de6090ee0f9a544702c9
Caitlan Spronk <caitlan.spronk@genesys.com>
2021-10-04 10:56:57 -0400

    feat(gux-chart): fix failing chart tests

--- src/components/beta/gux-chart-column/tests/gux-column-chart.spec.tsx
+++ src/components/beta/gux-chart-column/tests/gux-column-chart.spec.tsx
@@ -17,7 +17,8 @@ const chartData = {
 
 describe('guxchart-column-beta', () => {
   it('should build', async () => {
-    const html = '<gux-chart-column-beta></gux-chart-column-beta>';
+    const html =
+      '<gux-chart-column-beta x-field-name="date" y-field-name="value"></gux-chart-column-beta>';
     const page = await newSpecPage({ components, html, language });
 
     expect(page.rootInstance).toBeInstanceOf(GuxColumnChart);
--- src/components/beta/gux-chart-line/tests/gux-chart-line.spec.tsx
+++ src/components/beta/gux-chart-line/tests/gux-chart-line.spec.tsx
@@ -23,7 +23,8 @@ const chartData = {
 
 describe('gux-chart-line-beta', () => {
   it('should build', async () => {
-    const html = '<gux-chart-line-beta></gux-chart-line-beta>';
+    const html =
+      '<gux-chart-line-beta x-field-name="date" y-field-name="value"></gux-chart-line-beta>';
     const page = await newSpecPage({ components, html, language });
 
     expect(page.rootInstance).toBeInstanceOf(GuxLineChart);
