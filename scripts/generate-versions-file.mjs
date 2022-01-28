17581f371bc2a7d2b1ceac0bc7736c00d828764a
Daragh King <daragh.king@genesys.com>
2022-01-28 11:03:54 +0000

    fix(scripts): fixed generate-versions-file script
    fixed generate-versions-file script
    
    COMUI-879

---
+++ scripts/generate-versions-file.mjs
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import fetch from 'node-fetch';
+import fs from 'fs';
+import path from 'path';
+import semverSort from 'semver-sort';
+import { createRequire } from 'module';
+
+const requireShim = createRequire(import.meta.url);
+
+const newVersion = requireShim('../package.json').version;
+const newVersionsPath = path.resolve('docs/dist/versions.json');
+const currentVersionsUrl = 'https://apps.inindca.com/common-ui-docs/genesys-webcomponents/versions.json';
+
+const currentVersionsFile = await fetch(currentVersionsUrl);
+const currentVersionsJson = await currentVersionsFile.json();
+
+const newVersionsJson = semverSort.desc(currentVersionsJson.concat(newVersion));
+const newVersionsString = JSON.stringify(newVersionsJson, null, 2);
+
+fs.writeFileSync(newVersionsPath, newVersionsString, { encoding: 'utf8' });
