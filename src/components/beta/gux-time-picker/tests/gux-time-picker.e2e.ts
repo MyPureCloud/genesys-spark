e8a05268e392edfe0f50f623b3ccb6bc02183fee
Daragh King <daragh.king@genesys.com>
2022-02-25 12:22:40 +0000

    feat(time-picker): enabled shadow dom
    enabled shadow dom
    
    COMUI-869

--- src/components/beta/gux-time-picker/tests/gux-time-picker.e2e.ts
+++ src/components/beta/gux-time-picker/tests/gux-time-picker.e2e.ts
@@ -1,10 +1,9 @@
 import { newSparkE2EPage, a11yCheck } from '../../../../../tests/e2eTestUtils';
 
+const html = '<gux-time-picker-beta lang="en"></gux-time-picker-beta>';
 describe('gux-time-picker-beta', () => {
   it('renders', async () => {
-    const page = await newSparkE2EPage({
-      html: `<gux-time-picker-beta></gux-time-picker-beta>`
-    });
+    const page = await newSparkE2EPage({ html });
     const element = await page.find('gux-time-picker-beta');
 
     expect(element).toHaveClass('hydrated');
@@ -12,103 +11,95 @@ describe('gux-time-picker-beta', () => {
 
   it('Active class on focus', async () => {
     // Setup
-    const page = await newSparkE2EPage({
-      html: `<gux-time-picker-beta></gux-time-picker-beta>`
-    });
+    const page = await newSparkE2EPage({ html });
+    const element = await page.find('gux-time-picker-beta');
+    const input = await element.find('pierce/input');
 
     // Run
-    await page.focus('input');
+    await input.focus();
     await page.waitForChanges();
 
     // Validate
-    const input = await page.find('gux-time-picker-beta input');
-    const focusedElement = await page.find(':focus');
+    const focusedElement = await element.find('pierce/:focus');
     expect(focusedElement.outerHTML).toEqual(input.outerHTML);
   });
 
   it('Type a valid value', async () => {
-    const page = await newSparkE2EPage({
-      html: `<gux-time-picker-beta></gux-time-picker-beta>`
-    });
+    const page = await newSparkE2EPage({ html });
+    const element = await page.find('gux-time-picker-beta');
+    const input = await element.find('pierce/input');
+
     await a11yCheck(page, [], 'dropdown closed');
 
     // Focus in
-    const input = await page.find('gux-time-picker-beta input');
     await input.focus();
-    let focusedElement = await page.find(':focus');
+    let focusedElement = await element.find('pierce/:focus');
     expect(focusedElement.outerHTML).toEqual(input.outerHTML);
 
     // Type
     await input.type('00:55:00');
-    let list = await page.find('gux-time-picker-beta gux-list');
+    let list = await element.find('pierce/gux-list');
     await a11yCheck(page, [], 'dropdown opened');
-    focusedElement = await page.find(':focus');
+    focusedElement = await element.find('pierce/:focus');
     expect(focusedElement.outerHTML).toEqual(input.outerHTML);
 
     expect(list).not.toBeNull();
 
     // Keep focus on input
     await input.press('Enter');
-    const timePicker = await page.find('gux-time-picker-beta');
-    list = await page.find('gux-time-picker-beta gux-list');
-    focusedElement = await page.find(':focus');
+    list = await element.find('pierce/gux-list');
+    focusedElement = await element.find('pierce/:focus');
     expect(focusedElement.outerHTML).toEqual(input.outerHTML);
     expect(list).toBeNull();
-    expect(await timePicker.getProperty('value')).toEqual('00:55:00');
+    expect(await element.getProperty('value')).toEqual('00:55:00');
   });
 
   it('Type an invalid value', async () => {
-    const page = await newSparkE2EPage({
-      html: `<gux-time-picker-beta></gux-time-picker-beta>`
-    });
+    const page = await newSparkE2EPage({ html });
+    const element = await page.find('gux-time-picker-beta');
+    const input = await element.find('pierce/input');
 
     // Focus in
-    const input = await page.find('gux-time-picker-beta input');
     await input.focus();
-    let focusedElement = await page.find(':focus');
+    let focusedElement = await element.find('pierce/:focus');
     expect(focusedElement.outerHTML).toEqual(input.outerHTML);
 
     // Type
     await input.type('00:55:0');
-    let list = await page.find('gux-time-picker-beta gux-list');
-    focusedElement = await page.find(':focus');
+    let list = await element.find('pierce/gux-list');
+    focusedElement = await element.find('pierce/:focus');
     expect(focusedElement.outerHTML).toEqual(input.outerHTML);
     // expect(input).toHaveFocus();
     expect(list).not.toBeNull();
 
     // Keep focus on input
     await input.press('Enter');
-    const timePicker = await page.find('gux-time-picker-beta');
-    list = await page.find('gux-time-picker-beta gux-list');
-    focusedElement = await page.find(':focus');
+    list = await element.find('pierce/gux-list');
+    focusedElement = await element.find('pierce/:focus');
     expect(focusedElement.outerHTML).toEqual(input.outerHTML);
     expect(list).toBeNull();
-    expect(await timePicker.getProperty('value')).toEqual('');
+    expect(await element.getProperty('value')).toEqual('');
   });
 
   it('Selects a value from the dropdown on click', async () => {
-    const page = await newSparkE2EPage({
-      html: `<gux-time-picker-beta></gux-time-picker-beta>`
-    });
+    const page = await newSparkE2EPage({ html });
+    const element = await page.find('gux-time-picker-beta');
+    const input = await element.find('pierce/input');
 
     // click input
-    const input = await page.find('gux-time-picker-beta input');
     await input.click();
-    const focusedElement = await page.find(':focus');
+    const focusedElement = await element.find('pierce/:focus');
     expect(focusedElement.outerHTML).toEqual(input.outerHTML);
 
     // Type
     await input.type('00:00:00');
-    const list = await page.find('gux-time-picker-beta gux-list');
-    const listItem = await page.findAll(
-      'gux-time-picker-beta gux-list gux-list-item'
-    );
+    const list = await element.find('pierce/gux-list');
+    const listItem = await element.findAll('pierce/gux-list gux-list-item');
 
     expect(list).not.toBeNull();
 
     // click list item
     await listItem[1].click();
-    const timePicker = await page.find('gux-time-picker-beta');
-    expect(await timePicker.getProperty('value')).toEqual('00:15:00');
+    expect(await element.getProperty('value')).toEqual('00:15:00');
   });
 });
