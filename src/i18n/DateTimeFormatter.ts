03daff73b69dc2cd41dcc1c729253dc2ec7b505e
Yu Xuan Ou <yu_xuan.ou@genesys.com>
2022-07-20 15:16:12 -0400

    docs(datetimeformatter): added documentation comments

--- src/i18n/DateTimeFormatter.ts
+++ src/i18n/DateTimeFormatter.ts
@@ -1,10 +1,20 @@
 import locales from './locales.json';
 
+/**
+ * Allowed formats for formatting dates and times 
+ */
 export type GuxDateTimeFormat = 'full' | 'long' | 'medium' | 'short';
 
+/**
+ * @class A class that implements the method needed to format dates and times depending on locales
+ */
 export class DateTimeFormatter {
     locale: typeof locales[number];
 
+    /**
+     * @constructor  
+     * @param locale A locale string is required to initialize a DateTimeFormatter class instance
+     */
     constructor(locale: typeof locales[number]) {
         if (locales.includes(locale)) {
             this.locale = locale;
@@ -14,14 +24,32 @@ export class DateTimeFormatter {
         }
     }
 
+    /**
+     * 
+     * @param {Date} datetime A Date object that requires formatting
+     * @param {GuxDateTimeFormat} format An allowed format string 
+     * @returns {string} Formatted date string
+     */
     formatDate(datetime: Date, format: GuxDateTimeFormat) : string {
         return datetime.toLocaleDateString(this.locale, { dateStyle: format });
     }
 
+    /**
+     * 
+     * @param {Date} datetime A Date object that requires formatting
+     * @param {GuxDateTimeFormat} format An allowed format string 
+     * @returns {string} Formatted time string
+     */
     formatTime(datetime: Date, format: GuxDateTimeFormat) : string {
         return datetime.toLocaleTimeString(this.locale, { timeStyle: format });
     }
 
+    /**
+     * 
+     * @param {Date} datetime A Date object that requires formatting
+     * @param {GuxDateTimeFormat} format An allowed format string 
+     * @returns {string} Formatted datetime string
+     */
     formatDateTime(datetime: Date, format: GuxDateTimeFormat) : string {
         return datetime.toLocaleString(this.locale, { dateStyle: format, timeStyle: format });
     }
