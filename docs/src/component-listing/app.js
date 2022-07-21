157a79e5e9dafbe1cca6067639aacaaa3e4ae12d
Katie Bobbe <katie.bobbe@genesys.com>
2022-07-21 16:21:14 -0400

    docs(links): open external links in new tab
    COMUI-1119

--- docs/src/component-listing/app.js
+++ docs/src/component-listing/app.js
@@ -38,9 +38,9 @@ export function bootstrap() {
                   .join('')}
                   <div class="sticky-footer">
                   <h2>Resources</h2>
-                    <a class="resources-link" href="https://github.com/MyPureCloud/genesys-webcomponents/tree/main/docs/migrations/v3">V2 -> V3 Migration Guide</a>
-                    <a class="resources-link" href="https://spark.genesys.com">Spark UX Documentation</a>
-                    <a class="resources-link" href="https://github.com/MyPureCloud/genesys-webcomponents/blob/main/README.md#genesys-web-components">Web Components README</a>
+                    <a class="resources-link" href="https://github.com/MyPureCloud/genesys-webcomponents/tree/main/docs/migrations/v3" target="_blank">V2 -> V3 Migration Guide</a>
+                    <a class="resources-link" href="https://spark.genesys.com" target="_blank">Spark UX Documentation</a>
+                    <a class="resources-link" href="https://github.com/MyPureCloud/genesys-webcomponents/blob/main/README.md#genesys-web-components" target="_blank">Web Components README</a>
                   </div>
             </nav>
             <iframe id="componentFrame" title="Component Examples"/>
