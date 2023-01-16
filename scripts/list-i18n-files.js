55b086a02a6a9b42d1a4b25afe1f86b45d140970
Daragh King <daragh.king@genesys.com>
2023-01-16 16:40:13 +0000

    refactor(localisation): removing unused translation files
    removing unused translation files
    
    COMUI-1429

--- scripts/list-i18n-files.js
+++ scripts/list-i18n-files.js
@@ -4,41 +4,8 @@ const fs = require('fs');
 const glob = require('glob');
 const locales = require('../src/i18n/locales.json');
 
-const files = glob.sync('src/components/**/i18n/en.json');
+const files = glob.sync('src/components/**/i18n/*.json');
 
 const output = files.map(file => {
-  const folder = file.replace('/en.json', '');
-  const component = folder.replace('/i18n', '').split('/').pop();
-
-  const englishFileRaw = fs.readFileSync(file, {
-    encoding: 'utf8',
-    flag: 'r'
-  });
-  const englishFileJson = JSON.parse(englishFileRaw);
-
-  const translationsFound = locales.reduce((acc, cv) => {
-    const otherLanguageFile = file.replace('/en.json', `/${cv}.json`);
-    const otherLanguageRaw = fs.readFileSync(otherLanguageFile, {
-      encoding: 'utf8',
-      flag: 'r'
-    });
-    const otherLanguageJson = JSON.parse(otherLanguageRaw);
-
-    const numberOfUntranslatedKeys = Object.keys(englishFileJson).reduce(
-      (count, key) => {
-        return englishFileJson[key] !== otherLanguageJson[key] || cv === 'en'
-          ? count
-          : count + 1;
-      },
-      0
-    );
-
-    return Object.assign(acc, {
-      [cv]: numberOfUntranslatedKeys ? numberOfUntranslatedKeys : '✅'
-    });
-  }, {});
-
-  return Object.assign({ component }, translationsFound);
+  console.log(file);
 });
-
-console.table(output, ['component'].concat(locales));
