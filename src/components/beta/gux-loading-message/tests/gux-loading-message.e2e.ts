6569bf174866a1950bb8501457c6c99b4e4fd5aa
Conor Darcy <conor.darcy@genesys.com>
2023-02-21 15:10:53 +0000

    test(axe): adding 2 axe test to increase coverage

--- src/components/beta/gux-loading-message/tests/gux-loading-message.e2e.ts
+++ src/components/beta/gux-loading-message/tests/gux-loading-message.e2e.ts
@@ -1,4 +1,4 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { newSparkE2EPage, a11yCheck } from '../../../../test/e2eTestUtils';
 
 describe('gux-loading-message-beta', () => {
   it('renders', async () => {
@@ -7,8 +7,10 @@ describe('gux-loading-message-beta', () => {
         <span slot="additional-guidance">Thank you for waiting.</span>
         <gux-radial-progress slot="progress" />
         </gux-loading-message-beta>`;
-    const page = await newE2EPage({ html });
+    const page = await newSparkE2EPage({ html });
     const element = await page.find('gux-loading-message-beta');
+
+    await a11yCheck(page);
 
     expect(element.innerHTML).toMatchSnapshot();
   });
