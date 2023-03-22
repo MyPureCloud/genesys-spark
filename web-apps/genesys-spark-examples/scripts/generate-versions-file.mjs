0f6c2e3f570491721481d37b8479b03e71479955
Daragh King <daragh.king@genesys.com>
2023-03-22 12:20:40 +0000

    fix(monorepo): added missing versions file generation
    added missing versions file generation
    
    COMUI-1590

---
+++ web-apps/genesys-spark-examples/scripts/generate-versions-file.mjs
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+import fetch from 'node-fetch';
+import fs from 'fs';
+import path from 'path';
+import semverSort from 'semver-sort';
+import { createRequire } from 'module';
+
+const requireShim = createRequire(import.meta.url);
+
+const newVersion = requireShim('../package.json').version;
+
+const currentVersionsUrl =
+  'https://apps.inindca.com/common-ui-docs/genesys-webcomponents/versions.json';
+const currentVersionsFile = await fetch(currentVersionsUrl);
+const currentVersionsJson = await currentVersionsFile.json();
+
+const npmPackageUrl = 'https://registry.npmjs.org/genesys-spark-components';
+const npmPackageFile = await fetch(npmPackageUrl);
+const npmPackageJson = await npmPackageFile.json();
+const npmVersionsJson = Object.keys(npmPackageJson.versions);
+
+const newVersionsJson = semverSort.desc(
+  [].concat(currentVersionsJson, npmVersionsJson, newVersion)
+);
+const newVersionsString = JSON.stringify(newVersionsJson, null, 2);
+
+fs.writeFileSync(path.resolve('./dist/versions.json'), newVersionsString, {
+  encoding: 'utf8'
+});
