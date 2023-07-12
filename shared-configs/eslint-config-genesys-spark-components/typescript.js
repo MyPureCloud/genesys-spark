399b8492b7ddffa7785eb98c082f1de939fe2a71
Daragh King <daragh.king@genesys.com>
2023-07-12 15:12:44 +0100

    chore(dependencies): updated dependencies
    updated dependencies
    
    COMUI-1703

--- shared-configs/eslint-config-genesys-spark-components/typescript.js
+++ shared-configs/eslint-config-genesys-spark-components/typescript.js
@@ -7,6 +7,8 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   rules: {
-    '@typescript-eslint/no-inferrable-types': 'off'
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-redundant-type-constituents': 'off',
+    '@typescript-eslint/no-unsafe-enum-comparison': 'off'
   }
 };
