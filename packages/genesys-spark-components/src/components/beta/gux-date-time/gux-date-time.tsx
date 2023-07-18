d6526275e4311fa1d964f5d90ff9e38bdeb69d29
Daragh King <daragh.king@genesys.com>
2023-07-18 18:10:11 +0100

    feat(date-time): added ability to set time zone
    added ability to set time zone
    
    COMUI-1852
    
    (cherry picked from commit e454bdf9c8f9ec6dbcb3affc1fc95eb0369259e7)

--- packages/genesys-spark-components/src/components/beta/gux-date-time/gux-date-time.tsx
+++ packages/genesys-spark-components/src/components/beta/gux-date-time/gux-date-time.tsx
@@ -5,6 +5,7 @@ import {
   GuxDateTimeFormat
 } from '../../../i18n/DateTimeFormatter';
 import { getDesiredLocale } from '../../../i18n/index';
+import { GuxTimeZoneIdentifier } from '../../../i18n/time-zone/types';
 
 @Component({
   tag: 'gux-date-time-beta',
@@ -31,6 +32,12 @@ export class GuxDateTime {
   @Prop()
   format: GuxDateTimeFormat = 'short';
 
+  /**
+   * Time zone identifier
+   */
+  @Prop()
+  timeZone: GuxTimeZoneIdentifier;
+
   componentWillLoad(): void {
     trackComponent(this.root);
     this.formatter = new DateTimeFormatter(getDesiredLocale(this.root));
@@ -39,7 +46,9 @@ export class GuxDateTime {
   render() {
     return (
       <Host>
-        {this.formatter.formatDateTime(new Date(this.datetime), this.format)}
+        {this.formatter.formatDateTime(new Date(this.datetime), this.format, {
+          timeZone: this.timeZone
+        })}
       </Host>
     ) as JSX.Element;
   }
