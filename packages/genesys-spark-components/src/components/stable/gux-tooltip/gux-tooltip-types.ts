IshanSandhu1/genesys-webcomponents

refactor(tooltip): wip
wip

COMUI-1688

packages/genesys-spark-components/src/components/stable/gux-tooltip/gux-tooltip-types.ts
@@ -1,0 +1,1 @@
+export type GuxTooltipContrast = 'light' | 'dark';

MyPureCloud/genesys-spark

packages/genesys-spark-components/src/components/stable/gux-tooltip/gux-tooltip-types.ts
@@ -1,0 +1,1 @@
+export type GuxTooltipAccent = 'light' | 'dark';
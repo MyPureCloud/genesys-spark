55a5a823361313a06274db89d666884ef97e2946
Gavin Everett <geverett@REM-GEVERETT-M.local>
2023-05-25 14:09:09 +0100

    refactor(tooltip): wip
    wip
    
    COMUI-1688

--- packages/genesys-spark-components/src/components/stable/gux-tooltip/gux-tooltip.tsx
+++ packages/genesys-spark-components/src/components/stable/gux-tooltip/gux-tooltip.tsx
@@ -15,24 +15,28 @@ import {
   flip,
   offset,
   shift,
-  Placement
+  Placement,
+  arrow
 } from '@floating-ui/dom';
 
 import { randomHTMLId } from '@utils/dom/random-html-id';
 import { trackComponent } from '@utils/tracking/usage';
 import { findElementById } from '@utils/dom/find-element-by-id';
 import { afterNextRender } from '@utils/dom/after-next-render';
+import { GuxTooltipAccent } from './gux-tooltip-types';
 
 /**
  * @slot - Content of the tooltip
  */
 @Component({
-  styleUrl: 'gux-tooltip.less',
+  styleUrl: 'gux-tooltip.scss',
   tag: 'gux-tooltip',
   shadow: true
 })
 export class GuxTooltip {
   private forElement: HTMLElement;
+  private arrowElement: HTMLDivElement;
+
   private pointerenterHandler: () => void = () => this.show();
   private pointerleaveHandler: () => void = () => this.hide();
   private focusinHandler: () => void = () => this.show();
@@ -54,6 +58,12 @@ export class GuxTooltip {
    */
   @Prop({ mutable: true })
   placement: Placement = 'bottom-start';
+
+  @Prop()
+  accent: GuxTooltipAccent = 'light';
+
+  @Prop()
+  anchor: boolean = false;
 
   /**
    * If tooltip is shown or not
@@ -101,17 +111,47 @@ export class GuxTooltip {
   }
 
   private updatePosition(): void {
+    const middleware = [offset(16), flip(), shift()];
+
+    if (this.anchor) {
+      middleware.push(arrow({ element: this.arrowElement }));
+    }
+
     void computePosition(this.forElement, this.root, {
       placement: this.placement,
       strategy: 'fixed',
-      middleware: [offset(16), flip(), shift()]
-    }).then(({ x, y, placement }) => {
+      middleware: middleware
+    }).then(({ x, y, middlewareData, placement }) => {
       Object.assign(this.root.style, {
         left: `${x}px`,
         top: `${y}px`
       });
-      // data-placement is currently only used for e2e tests
-      this.root.setAttribute('data-placement', placement);
+      //data-placement needed on the for e2e tests.
+      this.root.setAttribute('data-placement', this.placement);
+
+      const side = placement.split('-')[0];
+
+      const staticSide = {
+        top: 'bottom',
+        right: 'left',
+        bottom: 'top',
+        left: 'right'
+      }[side];
+
+      if (middlewareData.arrow) {
+        //This is 13 because this makes the arrow look aligned.
+        const arrowLen = 13;
+        const { x, y } = middlewareData.arrow;
+
+        Object.assign(this.arrowElement.style, {
+          left: x != null ? `${x}px` : '',
+          top: y != null ? `${y}px` : '',
+          right: '',
+          bottom: '',
+          [staticSide]: `${-arrowLen / 2}px`,
+          transform: 'rotate(45deg)'
+        });
+      }
     });
   }
   private show(): void {
@@ -144,6 +184,17 @@ export class GuxTooltip {
     }
   }
 
+  private renderAnchor(): JSX.Element {
+    if (this.anchor) {
+      return (
+        <div
+          ref={(el: HTMLDivElement) => (this.arrowElement = el)}
+          class="gux-arrow"
+        ></div>
+      ) as JSX.Element;
+    }
+  }
+
   connectedCallback(): void {
     this.getForElement();
 
@@ -166,7 +217,7 @@ export class GuxTooltip {
   }
 
   componentWillLoad(): void {
-    trackComponent(this.root);
+    trackComponent(this.root, { variant: this.placement });
   }
 
   disconnectedCallback(): void {
@@ -189,8 +240,15 @@ export class GuxTooltip {
   render(): JSX.Element {
     return (
       <Host id={this.id} class={{ 'gux-show': this.isShown }} role="tooltip">
-        <div class="gux-container">
+        <div
+          class={{
+            'gux-container': true,
+            [`gux-${this.accent}`]: true
+          }}
+          data-placement={this.placement}
+        >
           <slot />
+          {this.renderAnchor()}
         </div>
       </Host>
     ) as JSX.Element;
