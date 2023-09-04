9fa65aef9ed4999e1088e80eba154fbbd6ad4104
Daragh King <daragh.king@genesys.com>
2023-09-04 15:20:05 +0100

    refactor(radial-progress): updated style to v4
    updated style to v4
    
    COMUI-2012

--- packages/genesys-spark-components/src/components/stable/gux-radial-progress/tests/gux-radial-progress.service.spec.ts
+++ packages/genesys-spark-components/src/components/stable/gux-radial-progress/tests/gux-radial-progress.service.spec.ts
@@ -27,27 +27,14 @@ describe('gux-radial-progress.service', () => {
 
   describe('#getPercentageString', () => {
     [
-      { value: 0, max: 10, scale: 0, expectResult: '0%' },
-      { value: 1, max: 10, scale: 0, expectResult: '10%' },
-      { value: 1.5, max: 10, scale: 0, expectResult: '15%' },
-      { value: 1.55, max: 10, scale: 0, expectResult: '16%' },
-      { value: 1.555, max: 10, scale: 0, expectResult: '16%' },
-
-      { value: 0, max: 10, scale: 1, expectResult: '0%' },
-      { value: 1, max: 10, scale: 1, expectResult: '10%' },
-      { value: 1.5, max: 10, scale: 1, expectResult: '15%' },
-      { value: 1.55, max: 10, scale: 1, expectResult: '15.5%' },
-      { value: 1.555, max: 10, scale: 1, expectResult: '15.6%' },
-
-      { value: 0, max: 10, scale: 2, expectResult: '0%' },
-      { value: 1, max: 10, scale: 2, expectResult: '10%' },
-      { value: 1.5, max: 10, scale: 2, expectResult: '15%' },
-      { value: 1.55, max: 10, scale: 2, expectResult: '15.5%' },
-      { value: 1.555, max: 10, scale: 2, expectResult: '15.55%' },
-      { value: 1.5555, max: 10, scale: 2, expectResult: '15.56%' }
-    ].forEach(({ value, max, scale, expectResult }, index) => {
+      { value: 0, max: 10, expectResult: '0%' },
+      { value: 1, max: 10, expectResult: '10%' },
+      { value: 1.5, max: 10, expectResult: '15%' },
+      { value: 1.55, max: 10, expectResult: '16%' },
+      { value: 1.5555, max: 10, expectResult: '16%' }
+    ].forEach(({ value, max, expectResult }, index) => {
       it(`should render component as expected (${index + 1})`, async () => {
-        const result = getPercentageString(value, max, scale);
+        const result = getPercentageString(value, max);
 
         expect(result).toBe(expectResult);
       });
