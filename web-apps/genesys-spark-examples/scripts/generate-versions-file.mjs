e49de1b93323f7aae2f3b79161305a94c6eef708
Daragh King <daragh.king@genesys.com>
2023-09-19 12:35:15 +0100

    fix(versions): removed prerelease versions from generated versions file
    removed prerelease versions from generated versions file
    
    COMUI-2268

--- web-apps/genesys-spark-examples/scripts/generate-versions-file.mjs
+++ web-apps/genesys-spark-examples/scripts/generate-versions-file.mjs
@@ -4,6 +4,7 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 import path from 'path';
 import semverSort from 'semver-sort';
+import { prerelease } from 'semver';
 import { createRequire } from 'module';
 
 const requireShim = createRequire(import.meta.url);
@@ -20,9 +21,11 @@ const npmPackageFile = await fetch(npmPackageUrl);
 const npmPackageJson = await npmPackageFile.json();
 const npmVersionsJson = Object.keys(npmPackageJson.versions);
 
-const newVersionsJson = semverSort.desc([
-  ...new Set([].concat(currentVersionsJson, npmVersionsJson, newVersion))
-]);
+const newVersionsJson = semverSort
+  .desc([
+    ...new Set([].concat(currentVersionsJson, npmVersionsJson, newVersion))
+  ])
+  .filter(v => !Boolean(prerelease(v)));
 const newVersionsString = JSON.stringify(newVersionsJson, null, 2);
 
 fs.writeFileSync(path.resolve('./dist/versions.json'), newVersionsString, {
