422e01fba0f53822bcc8fbdd23b31db2695dfe85
321gillian <gillian.middleton@gmail.com>
2023-12-06 14:08:44 +0000

    feat(migration guide links): point migration guide links to the right place
    Point migration guide links to the right place

--- web-apps/genesys-spark-examples/src/component-listing/app.js
+++ web-apps/genesys-spark-examples/src/component-listing/app.js
@@ -40,7 +40,7 @@ export async function bootstrap() {
                   .join('')}
                   <div class="sticky-footer">
                   <h2>Resources</h2>
-                    <a class="resources-link" href="https://github.com/MyPureCloud/genesys-spark/tree/main/packages/genesys-spark-components/documentation/migrations/v4" target="_blank">V3 -> V4 Migration Guide</a>
+                    <a class="resources-link" href="https://github.com/MyPureCloud/genesys-spark/blob/main/docs/migration-guides/v4/readme.md" target="_blank">V3 -> V4 Migration Guide</a>
                     <a class="resources-link" href="https://spark.genesys.com" target="_blank">Spark 4.0 UX Documentation</a>
                     <a class="resources-link" href="https://github.com/MyPureCloud/genesys-spark/blob/main/README.md#genesys-web-components" target="_blank">Genesys Spark Components README</a>
                   </div>
