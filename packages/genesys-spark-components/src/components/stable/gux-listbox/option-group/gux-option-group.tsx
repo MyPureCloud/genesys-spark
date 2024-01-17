da7a62e5f967a8e456e8d2e53320eaa56c1ea536
321gillian <gillian.middleton@gmail.com>
2024-01-17 17:35:53 +0000

    feat(tidy implmentation): tidy implmentation
    GDS-2325

--- packages/genesys-spark-components/src/components/stable/gux-listbox/option-group/gux-option-group.tsx
+++ packages/genesys-spark-components/src/components/stable/gux-listbox/option-group/gux-option-group.tsx
@@ -25,13 +25,10 @@ export class GuxOptionGroup {
   // TOOD: Add disabled state once design work is complete
 
   @State()
-  private parentFilterValue: string = '';
-
-  @State()
   filtered: boolean = false;
 
   @State()
-  hideDivider: boolean = false;
+  showDivider: boolean = true;
 
   componentDidLoad(): void {
     this.rootParent = getClosestElement(
@@ -40,27 +37,20 @@ export class GuxOptionGroup {
     ) as HTMLElement;
 
     this.parentObserver = onMutation(this.rootParent, () => {
-      this.hideDivider =
-        this.parentFilterValue.length > 0 &&
-        !this.hasVisibleGroupAfter(this.root);
-    });
+      const groupOptions = Array.from(this.root?.children).filter(child =>
+        this.isOption(child)
+      ) as ListboxOptionElement[];
+      const allOptionsFiltered = groupOptions.every(option => option.filtered);
 
-    if (this.rootParent) {
-      this.rootParent.addEventListener(
-        'guxfilter',
-        this.handleFilterChange.bind(this) as EventListenerOrEventListenerObject
-      );
-    }
+      this.showDivider =
+        this.hasVisibleNextSibling(this.root) && !allOptionsFiltered;
+
+      this.filtered = allOptionsFiltered;
+    });
   }
 
   disconnectedCallback(): void {
-    if (this.rootParent) {
-      this.rootParent.removeEventListener(
-        'guxfilter',
-        this.handleFilterChange.bind(this) as EventListenerOrEventListenerObject
-      );
-      this.parentObserver.disconnect();
-    }
+    this.parentObserver.disconnect();
   }
 
   private isOption(item: Element): boolean {
@@ -68,20 +58,7 @@ export class GuxOptionGroup {
     return optionTypes.includes(item.tagName);
   }
 
-  handleFilterChange(event: CustomEvent<string>) {
-    this.parentFilterValue = event.detail;
-
-    const options = Array.from(this.root?.children).filter(child =>
-      this.isOption(child)
-    ) as ListboxOptionElement[];
-    const allOptionsFiltered = !options.some(option =>
-      this.matchOption(option, this.parentFilterValue)
-    );
-
-    this.filtered = this.parentFilterValue.length > 0 && allOptionsFiltered;
-  }
-
-  private hasVisibleGroupAfter(element: Element): boolean {
+  private hasVisibleNextSibling(element: Element): boolean {
     let nextOption = element.nextElementSibling;
     if (nextOption === null) return false;
     while (nextOption && nextOption.classList.contains('gux-filtered')) {
@@ -90,32 +67,23 @@ export class GuxOptionGroup {
     return Boolean(nextOption);
   }
 
-  private matchOption(
-    option: ListboxOptionElement,
-    matchString: string
-  ): boolean {
-    return option.textContent
-      .trim()
-      .toLowerCase()
-      .startsWith(matchString.toLowerCase());
+  renderDivider(): JSX.Element {
+    if (this.showDivider) {
+      return (<gux-list-divider></gux-list-divider>) as JSX.Element;
+    }
   }
 
   render(): JSX.Element {
     return (
       <Host class={{ 'gux-filtered': this.filtered }}>
-        <div
-          class={{
-            'gux-option-group': true,
-            'gux-hide-divider': this.hideDivider
-          }}
-        >
+        <div class="gux-option-group">
           <div class="gux-option-group-label" role="presentation">
             {this.label}
           </div>
           <div role="group">
             <slot />
           </div>
-          <gux-list-divider></gux-list-divider>
+          {this.renderDivider()}
         </div>
       </Host>
     ) as JSX.Element;
