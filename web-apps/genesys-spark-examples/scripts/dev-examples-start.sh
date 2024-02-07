30b83260f0b589c0385820c0c3c58b7fff905a5c
Katie Bobbe <katie.bobbe@genesys.com>
2024-02-07 11:37:07 -0700

    chore(build): component specs in build folder

--- web-apps/genesys-spark-examples/scripts/dev-examples-start.sh
+++ web-apps/genesys-spark-examples/scripts/dev-examples-start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-components_file_path="../../packages/genesys-spark-components/dist/component-specs.json"
-chart_components_file_path="../../packages/genesys-spark-chart-components/dist/component-specs.json"
+components_file_path="../../packages/genesys-spark-components/build/component-specs/component-specs.json"
+chart_components_file_path="../../packages/genesys-spark-chart-components/build/component-specs/component-specs.json"
 
 until [[ -f $components_file_path && -f $chart_components_file_path ]];
 do
   echo "Waiting for the component-specs files to be created..."
   sleep 1
 done
-echo "The component-specs files exist. Proceeding..."
+echo "The component-specs files exist. Starting example app..."
