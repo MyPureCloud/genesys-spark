b9c1e5182e85527098ab5bf5cc3dad1dfc504ce7
Daragh King <daragh.king@genesys.com>
2024-02-09 12:24:19 +0000

    fix(toolbar-custom-action): fixed click event firing when disabled
    fixed click event firing when disabled
    
    COMUI-2302

--- packages/genesys-spark-components/src/components/stable/gux-table-toolbar/gux-table-toolbar-custom-action/gux-table-toolbar-custom-action.tsx
+++ packages/genesys-spark-components/src/components/stable/gux-table-toolbar/gux-table-toolbar-custom-action/gux-table-toolbar-custom-action.tsx
@@ -1,4 +1,4 @@
-import { Component, Element, JSX, h, Prop } from '@stencil/core';
+import { Component, Element, JSX, h, Listen, Prop } from '@stencil/core';
 import { trackComponent } from '@utils/tracking/usage';
 import { GuxTableToolbarActionAccent } from '../gux-table-toolbar-action-accents.types';
 import { getSlotTextContent } from '@utils/dom/get-slot-text-content';
@@ -24,6 +24,14 @@ export class GuxTableToolbarCustomAction {
 
   @Prop()
   disabled: boolean = false;
+
+  @Listen('click', { capture: true })
+  handleClick(event: MouseEvent): void {
+    if (this.disabled) {
+      event.preventDefault();
+      event.stopImmediatePropagation();
+    }
+  }
 
   private renderTooltip(): JSX.Element {
     if (this.iconOnly) {
--- packages/genesys-spark-components/src/components/stable/gux-table-toolbar/gux-table-toolbar-custom-action/tests/gux-table-toolbar-custom-action.spec.ts
+++ packages/genesys-spark-components/src/components/stable/gux-table-toolbar/gux-table-toolbar-custom-action/tests/gux-table-toolbar-custom-action.spec.ts
@@ -4,39 +4,24 @@ import { GuxTableToolbarCustomAction } from '../gux-table-toolbar-custom-action'
 const components = [GuxTableToolbarCustomAction];
 const language = 'en';
 
-describe('click', () => {
-  it('should fire a click event when the toolbar action is clicked', async () => {
-    const html =
-      '<gux-table-toolbar-custom-action></gux-table-toolbar-custom-action>';
-    const page = await newSpecPage({ components, html, language });
-    const element = page.root as HTMLElement;
-    const clickSpy = jest.fn();
+describe('gux-table-toolbar-custom-action', () => {
+  describe('#render', () => {
+    [
+      {
+        description: 'should render a toolbar-custom-action',
+        html: '<gux-table-toolbar-custom-action><span slot="text">Filter</span><gux-icon slot="icon" icon-name="filter" decorative></gux-icon></gux-table-toolbar-custom-action>'
+      },
+      {
+        description: 'should render disabled toolbar-custom-action',
+        html: '<gux-table-toolbar-custom-action disabled ><span slot="text">Filter</span><gux-icon slot="icon" icon-name="filter" decorative></gux-icon></gux-table-toolbar-custom-action>'
+      }
+    ].forEach(({ description, html }) => {
+      it(description, async () => {
+        const page = await newSpecPage({ components, html, language });
 
-    element.addEventListener('click', clickSpy);
-
-    element.click();
-    await page.waitForChanges();
-
-    expect(clickSpy).toHaveBeenCalled();
-  });
-});
-
-describe('#render', () => {
-  [
-    {
-      description: 'should render a toolbar-custom-action',
-      html: '<gux-table-toolbar-custom-action><span slot="text">Filter</span><gux-icon slot="icon" icon-name="filter" decorative></gux-icon></gux-table-toolbar-custom-action>'
-    },
-    {
-      description: 'should render disabled toolbar-custom-action',
-      html: '<gux-table-toolbar-custom-action disabled ><span slot="text">Filter</span><gux-icon slot="icon" icon-name="filter" decorative></gux-icon></gux-table-toolbar-custom-action>'
-    }
-  ].forEach(({ description, html }) => {
-    it(description, async () => {
-      const page = await newSpecPage({ components, html, language });
-
-      expect(page.rootInstance).toBeInstanceOf(GuxTableToolbarCustomAction);
-      expect(page.root).toMatchSnapshot();
+        expect(page.rootInstance).toBeInstanceOf(GuxTableToolbarCustomAction);
+        expect(page.root).toMatchSnapshot();
+      });
     });
   });
 });
