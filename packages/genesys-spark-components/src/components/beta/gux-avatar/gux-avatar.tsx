df44b73476c8fddcbc30833e865ac82579508a68
321gillian <gillian.middleton@gmail.com>
2024-02-12 17:14:44 +0000

    Revert "feat(avatar): try abbr as prop"
    This reverts commit 88949922dd358f829cd2997d4b2f210fb158bf97.

--- packages/genesys-spark-components/src/components/beta/gux-avatar/gux-avatar.tsx
+++ packages/genesys-spark-components/src/components/beta/gux-avatar/gux-avatar.tsx
@@ -26,6 +26,14 @@ export class GuxAvatar {
   @Prop()
   name!: string;
 
+  private generateInitials(): string {
+    const nameArray = this.name?.split(' ') ?? [];
+    if (nameArray.length > 1) {
+      return nameArray[0].charAt(0) + nameArray[nameArray.length - 1].charAt(0);
+    }
+    return nameArray[0]?.charAt(0) + nameArray[0]?.charAt(1);
+  }
+
   private validatingInputs(): void {
     const avatarImage = this.root.querySelector('img');
     if (!this.name) {
@@ -59,7 +67,7 @@ export class GuxAvatar {
         <div class="gux-content">
           <slot name="image">
             {!this.root.querySelector('[slot="image"]') && (
-              <abbr title={this.name}>{this.abbr}</abbr>
+              <abbr title={this.name}>{this.generateInitials()}</abbr>
             )}
           </slot>
         </div>
