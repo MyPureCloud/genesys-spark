745d202e71e88f6e603d7b77e767eed2d14aed58
321gillian <gillian.middleton@gmail.com>
2024-02-13 12:52:51 +0000

    feat(avatar): tidy css

--- packages/genesys-spark-components/src/components/beta/gux-avatar/gux-avatar.tsx
+++ packages/genesys-spark-components/src/components/beta/gux-avatar/gux-avatar.tsx
@@ -54,14 +54,11 @@ export class GuxAvatar {
   }
 
   render(): JSX.Element {
-    const validSizes: GuxAvatarSize[] = ['xsmall', 'small', 'medium', 'large'];
-    const checkedSize = validSizes.includes(this.size) ? this.size : 'large';
-
     return (
       <div
         class={{
           'gux-avatar': true,
-          [`gux-${checkedSize}`]: true
+          [`gux-${this.size}`]: true
         }}
       >
         <div class="gux-content">
