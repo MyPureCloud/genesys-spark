b574b552c86d28912edbba5029a3fdef611c1627
Caitlan Spronk <caitlan.spronk@genesys.com>
2024-05-02 16:26:49 -0400

    fix(visualization): Add chart click handling to all chart types
    Add the same click handling that already exists for column charts to the
    other chart types
    
    ✅ Closes: ENGAGEUI-8215

--- packages/genesys-spark-chart-components/src/components/beta/gux-visualization/gux-visualization.tsx
+++ packages/genesys-spark-chart-components/src/components/beta/gux-visualization/gux-visualization.tsx
@@ -66,7 +66,12 @@ export class GuxVisualization {
         visSpec.signals.push({
           name: 'chartClick',
           value: 0,
-          on: [{ events: 'rect:mousedown', update: 'datum' }]
+          on: [
+            { events: 'rect:mousedown', update: 'datum' },
+            { events: 'arc:mousedown', update: 'datum' },
+            { events: 'line:mousedown', update: 'datum' },
+            { events: 'symbol:mousedown', update: 'datum' }
+          ]
         });
         return visSpec;
       }
