2eeaa2efefbcd37d11a491e03f63ef480f2a821a
Daragh King <daragh.king@genesys.com>
2024-05-30 15:34:39 +0100

    refactor(other): updated token build tools
    updated token build tools
    
    ✅ Closes: COMUI-2907

--- packages/genesys-spark-tokens/utils/output-styles.mjs
+++ packages/genesys-spark-tokens/utils/output-styles.mjs
@@ -2,6 +2,37 @@ import { temporaryWriteTask } from 'tempy';
 import StyleDictionary from 'style-dictionary';
 import { camelCase } from 'change-case';
 
+import Color from 'tinycolor2';
+
+function isColor(token) {
+  return Color(token.value).isValid();
+}
+
+StyleDictionary.registerTransform({
+  name: 'name/gse',
+  type: 'name',
+  transitive: false,
+  filter: () => true,
+  transform: (token, options) => {
+    return [options.prefix].concat(token.path.map(camelCase)).join('-');
+  }
+});
+
+StyleDictionary.registerTransform({
+  name: 'color/gse',
+  type: 'value',
+  transitive: false,
+  filter: isColor,
+  transform: function (token) {
+    const color = Color(token.original.value);
+    if (color.getAlpha() === 1) {
+      return color.toHexString();
+    } else {
+      return color.toHex8String();
+    }
+  }
+});
+
 export function outputStyles(
   rootFolder,
   setName,
@@ -20,82 +51,76 @@ export function outputStyles(
   return temporaryWriteTask(
     JSON.stringify(styleDictionaryReadableTokens),
     tempPath => {
-      const styleDictionary = StyleDictionary.extend({
-        source: [tempPath],
-        transform: {
-          'name/gse': {
-            type: 'name',
-            transitive: false,
-            matcher: () => true,
-            transformer: (token, options) => {
-              return [options.prefix]
-                .concat(token.path.map(camelCase))
-                .join('-');
+      const styleDictionary = new StyleDictionary(
+        {
+          source: [tempPath],
+          platforms: {
+            css: {
+              transformGroup: 'css',
+              transforms: ['name/gse', 'size/px', 'color/gse'],
+              prefix,
+              buildPath: `${rootFolder}/css/`,
+              files: [
+                {
+                  destination: `${destinationFilename}.css`,
+                  format: 'css/variables'
+                }
+              ],
+              options: {
+                showFileHeader: false
+              }
+            },
+            less: {
+              transformGroup: 'less',
+              transforms: ['name/gse', 'size/px', 'color/gse'],
+              prefix,
+              buildPath: `${rootFolder}/less/`,
+              files: [
+                {
+                  destination: `${destinationFilename}.less`,
+                  format: 'less/variables'
+                }
+              ],
+              options: {
+                showFileHeader: false,
+                outputReferences: false
+              }
+            },
+            scss: {
+              transformGroup: 'scss',
+              transforms: ['name/gse', 'size/px', 'color/gse'],
+              prefix,
+              buildPath: `${rootFolder}/scss/`,
+              files: [
+                {
+                  destination: `_${destinationFilename}.scss`,
+                  format: 'scss/variables'
+                }
+              ],
+              options: {
+                showFileHeader: false
+              }
+            },
+            json: {
+              transformGroup: 'js',
+              transforms: ['size/px', 'color/gse'],
+              buildPath: `${rootFolder}/json/`,
+              files: [
+                {
+                  destination: `${destinationFilename}.json`,
+                  format: 'json/nested'
+                }
+              ],
+              options: {
+                showFileHeader: false
+              }
             }
           }
         },
-        platforms: {
-          css: {
-            transformGroup: 'css',
-            transforms: ['name/gse'],
-            prefix,
-            buildPath: `${rootFolder}/css/`,
-            files: [
-              {
-                destination: `${destinationFilename}.css`,
-                format: 'css/variables'
-              }
-            ],
-            options: {
-              showFileHeader: false
-            }
-          },
-          less: {
-            transformGroup: 'less',
-            transforms: ['name/gse'],
-            prefix,
-            buildPath: `${rootFolder}/less/`,
-            files: [
-              {
-                destination: `${destinationFilename}.less`,
-                format: 'less/variables'
-              }
-            ],
-            options: {
-              showFileHeader: false,
-              outputReferences: false
-            }
-          },
-          scss: {
-            transformGroup: 'scss',
-            transforms: ['name/gse'],
-            prefix,
-            buildPath: `${rootFolder}/scss/`,
-            files: [
-              {
-                destination: `_${destinationFilename}.scss`,
-                format: 'scss/variables'
-              }
-            ],
-            options: {
-              showFileHeader: false
-            }
-          },
-          json: {
-            transformGroup: 'js',
-            buildPath: `${rootFolder}/json/`,
-            files: [
-              {
-                destination: `${destinationFilename}.json`,
-                format: 'json/nested'
-              }
-            ],
-            options: {
-              showFileHeader: false
-            }
-          }
+        {
+          verbosity: 'verbose'
         }
-      });
+      );
 
       styleDictionary.buildAllPlatforms();
     },
