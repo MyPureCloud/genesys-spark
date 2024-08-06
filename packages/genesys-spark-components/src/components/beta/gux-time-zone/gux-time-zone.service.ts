1e6b3e1c3c68e154d7a7a529c6569c8130a474e6
321gillian <gillian.middleton@genesys.com>
2024-08-06 16:57:28 +0100

    feat(time-zone): Add gux-time-zone to v4 (#549)
    ✅ Closes: COMUI-2956

---
+++ packages/genesys-spark-components/src/components/beta/gux-time-zone/gux-time-zone.service.ts
@@ -0,0 +1,19 @@
+import { GuxTimeZoneIdentifier } from '../../../i18n/time-zone/types';
+import { getTimeZoneList, formatOffset } from '../../../utils/date/time-zone';
+import { GuxTimeZoneListing } from '../gux-time-zone-picker/gux-time-zone-picker.types';
+
+export function shortenZone(zone: string): string {
+  const sections = zone.split('/');
+  return sections?.pop() || zone;
+}
+
+export function getLocalizedOffset(
+  localizedUTC: string,
+  timeZoneId: GuxTimeZoneIdentifier
+): string {
+  const zoneList: GuxTimeZoneListing[] = getTimeZoneList();
+  const timeZone = zoneList.find(zone => zone.name === timeZoneId);
+
+  const formattedOffset = formatOffset(timeZone?.currentTimeOffsetInMinutes);
+  return `${localizedUTC}${formattedOffset}`;
+}
