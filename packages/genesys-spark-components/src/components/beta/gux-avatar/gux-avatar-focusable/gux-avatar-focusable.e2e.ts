a5e3bf78fb74395ad1eb87002523c7908310aebf
321gillian <gillian.middleton@genesys.com>
2024-09-11 17:59:31 +0100

    feat(avatar): add tooltip (#594)
    ✅ Closes: COMUI-3041

--- packages/genesys-spark-components/src/components/beta/gux-avatar/gux-avatar-focusable/gux-avatar-focusable.e2e.ts
+++ packages/genesys-spark-components/src/components/beta/gux-avatar/gux-avatar-focusable/gux-avatar-focusable.e2e.ts
@@ -1,4 +1,4 @@
-import { newSparkE2EPage, a11yCheck } from '../../../test/e2eTestUtils';
+import { newSparkE2EPage, a11yCheck } from '../../../../test/e2eTestUtils';
 
 describe('gux-avatar', () => {
   describe('#render', () => {
