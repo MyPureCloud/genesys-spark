5c3dd963fc834f294857d3b98d9695edccf473a7
Daragh King <daragh.king@genesys.com>
2024-10-01 14:55:28 +0100

    fix(visualization): added more localizations
    added more localizations
    
    ✅ Closes: COMUI-2168

---
+++ packages/genesys-spark-chart-components/src/genesys-spark-utils/get-closest-element.ts
@@ -0,0 +1,23 @@
+//https://inindca.atlassian.net/browse/COMUI-2673
+// utility to get the closest element passing shadow dom boundaries
+export function getClosestElement(baseElement: HTMLElement, selector: string) {
+  function closest(
+    element: Element | Window | Document | null
+  ): Element | null {
+    if (!element || element === document || element === window) {
+      return null;
+    }
+
+    if ((element as Element).assignedSlot) {
+      element = (element as Element).assignedSlot;
+    }
+
+    const found = (element as Element).closest(selector);
+
+    return found
+      ? found
+      : closest(((element as Element).getRootNode() as ShadowRoot).host);
+  }
+
+  return closest(baseElement);
+}
