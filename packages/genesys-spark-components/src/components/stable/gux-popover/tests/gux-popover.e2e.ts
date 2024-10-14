c1f97a8e8b1897b8e3fe0a0e0df6bf11cab8d5a1
Thomas Dillon <thomas.dillon@genesys.com>
2024-10-14 08:53:19 -0500

    fix(popover): fix duplicate guxdismiss event when simultaneously clicking outside and focusout (#634)
    ✅ Closes: COMUI-3102

--- packages/genesys-spark-components/src/components/stable/gux-popover/tests/gux-popover.e2e.ts
+++ packages/genesys-spark-components/src/components/stable/gux-popover/tests/gux-popover.e2e.ts
@@ -42,6 +42,36 @@ describe('gux-popover', () => {
     expect(guxdismiss).toHaveReceivedEvent();
   });
 
+  it('should trigger one guxdismiss event when click outside causes focus out', async () => {
+    const page = await newSparkE2EPage({
+      html: `
+      <div lang="en">
+        <button id="focusable-element">
+          Example Focusable
+        </button>
+        <div id="popover-target">
+          Example Element
+        </div>
+        <gux-popover position="top" for="popover-target" display-dismiss-button close-on-click-outside is-open>
+          <div>
+            <button id="popover-content">
+              popover content
+            </button>
+          </div>
+        </gux-popover>
+      </div>
+      `
+    });
+
+    const component = await page.find('gux-popover');
+    const guxdismiss = await component.spyOnEvent('guxdismiss');
+    const content = await page.find('#popover-content');
+    content.focus();
+    const button = await page.find('#focusable-element');
+    await button.click();
+    expect(guxdismiss).toHaveReceivedEventTimes(1);
+  });
+
   it('Supports hiding the close button', async () => {
     const page = await newSparkE2EPage({
       html: `
