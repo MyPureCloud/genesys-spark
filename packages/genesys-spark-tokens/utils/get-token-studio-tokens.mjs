b5792ccedaed1fd3223d04980ac64941bcd7c327
Daragh King <daragh.king@genesys.com>
2024-10-15 16:41:15 +0100

    feat(other): remove old token build infrastructure
    remove old token build infrastructure
    
    ✅ Closes: COMUI-3173

--- packages/genesys-spark-tokens/utils/get-token-studio-tokens.mjs
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-export function getTokenStudioTokens(tokenStudioTokensFile) {
-  const tokens = JSON.parse(fs.readFileSync(tokenStudioTokensFile));
-
-  delete tokens.$themes;
-
-  return tokens;
-}
