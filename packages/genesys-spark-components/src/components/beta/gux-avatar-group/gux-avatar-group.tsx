b6a3813a3fd1e5e7ef894b05d8f65f4490519acc
321gillian <gillian.middleton@gmail.com>
2024-10-22 10:48:19 +0100

    chore(avatar-group): remove manual item creation and removal

--- packages/genesys-spark-components/src/components/beta/gux-avatar-group/gux-avatar-group.tsx
+++ packages/genesys-spark-components/src/components/beta/gux-avatar-group/gux-avatar-group.tsx
@@ -4,19 +4,27 @@ import {
   JSX,
   Element,
   State,
-  Prop,
-  writeTask
+  Prop
+  // writeTask
 } from '@stencil/core';
-import { afterNextRenderTimeout } from '@utils/dom/after-next-render';
 import { trackComponent } from '@utils/tracking/usage';
-import { GuxAvatarAccent } from '../gux-avatar/gux-avatar.types';
-import { randomHTMLId } from '@utils/dom/random-html-id';
+import { GuxAvatarAccent, GuxAvatarSize } from '../gux-avatar/gux-avatar.types';
+
+interface ProcessedAvatar {
+  focusableAvatar: HTMLGuxAvatarFocusableBetaElement;
+  avatar: HTMLGuxAvatarBetaElement;
+  button?: HTMLButtonElement;
+  link?: HTMLAnchorElement;
+  img?: HTMLImageElement;
+  name: string;
+  accent: GuxAvatarAccent;
+  isOverflow: boolean;
+}
 
 /**
  * @slot - Some gux-avatars
  */
 
-const MAX_AVATARS = 7;
 @Component({
   styleUrl: 'gux-avatar-group.scss',
   tag: 'gux-avatar-group-beta',
@@ -35,38 +43,17 @@ export class GuxAvatarGroup {
   @State()
   avatarList: HTMLGuxAvatarFocusableBetaElement[];
 
+  @State()
+  private processedAvatars: ProcessedAvatar[] = [];
+
   get countAvatars() {
     return this.avatarList.length ?? 0;
   }
 
-  get overflowCount() {
-    return MAX_AVATARS < this.avatarList.length
-      ? this.avatarList.length - MAX_AVATARS
-      : 0;
-  }
-
   async componentWillLoad(): Promise<void> {
-    this.avatarList = Array.from(
-      this.root.children
-    ) as HTMLGuxAvatarFocusableBetaElement[];
-
-    this.avatarList.forEach(focusableAvatar => {
-      const avatar = focusableAvatar.querySelector(
-        'gux-avatar-beta'
-      ) as HTMLGuxAvatarBetaElement;
-      avatar.size = 'small';
-      focusableAvatar.id = focusableAvatar.id
-        ? focusableAvatar.id
-        : randomHTMLId(`gux-avatar-list-item`);
-    });
+    this.processAvatars();
 
     trackComponent(this.root);
-  }
-
-  componentDidLoad() {
-    afterNextRenderTimeout(() => {
-      this.updateGroupItems();
-    }, 250);
   }
 
   disconnectedCallback() {
@@ -76,149 +63,139 @@ export class GuxAvatarGroup {
   }
 
   private getAccent(index): string {
-    return ((index % 12) + 1).toString();
+    return `${(index % 12) + 1}` as GuxAvatarAccent;
   }
 
-  private updateGroupItems(): void {
-    this.avatarList.forEach((avatar, index) => {
-      const shouldHide = index >= MAX_AVATARS;
-      const isHidden = avatar.classList.contains('gux-hide');
+  private processAvatars(): void {
+    const avatarElements = Array.from(
+      this.root.children
+    ) as HTMLGuxAvatarFocusableBetaElement[];
 
-      if (shouldHide && !isHidden) {
-        this.addItemToPopup(avatar);
-        avatar.classList.add('gux-hide');
-      } else if (!shouldHide && isHidden) {
-        this.removeItemFromPopup(avatar);
-        avatar.classList.remove('gux-hide');
-      }
+    this.processedAvatars = avatarElements.map((focusableAvatar, index) => {
+      const avatar = focusableAvatar.querySelector(
+        'gux-avatar-beta'
+      ) as HTMLGuxAvatarBetaElement;
+      const button = focusableAvatar.querySelector('button');
+      const link = focusableAvatar.querySelector('a');
+      const img = avatar.querySelector('img');
+      return {
+        focusableAvatar,
+        avatar,
+        button,
+        link,
+        img,
+        name: avatar.name,
+        accent: this.getAccent(index),
+        isOverflow: index >= this.avatarLimit
+      } as ProcessedAvatar;
     });
   }
 
-  private addItemToPopup(
-    focusableAvatar: HTMLGuxAvatarFocusableBetaElement
-  ): void {
-    // Clone the original focusableAvatar so it can be used in overflow popup
-    const clonedFocusableAvatar = focusableAvatar?.cloneNode(
-      true
-    ) as HTMLGuxAvatarFocusableBetaElement;
-
-    const list = this.root.shadowRoot.querySelector('gux-list');
-
-    // Create a new list item with the same id as the focusableAvatar
-    const listItem = document.createElement('gux-list-item');
-
-    listItem.id = focusableAvatar.id;
-
-    // Give it a wrapper with a flexbox class
-    const listItemFlexbox = document.createElement('div');
-    listItemFlexbox.classList.add('gux-overflow-list-item');
-
-    // Get the button or link within the cloned focusableAvatar
-    // and set its tabIndex to -1 so it can't be tabbed to
-    const buttonOrLink = clonedFocusableAvatar.querySelector('button, a');
-    if (buttonOrLink) {
-      buttonOrLink.setAttribute('tabIndex', '-1');
-    }
-
-    // Append the avatar's name as text
-    if (clonedFocusableAvatar) {
-      // Get the nested avatar
-      const avatar = clonedFocusableAvatar.querySelector(
-        'gux-avatar-beta'
-      ) as HTMLGuxAvatarBetaElement;
-
-      if (avatar) {
-        // Set the accent
-        avatar.accent = this.getAccent(
-          this.avatarList.indexOf(focusableAvatar)
-        ) as GuxAvatarAccent;
-
-        // Set the size
-        avatar.size = 'xsmall';
-
-        // Create a span with the name from the avatar
-        const nameSpan = document.createElement('span');
-        nameSpan.classList.add('gux-avatar-name');
-        nameSpan.textContent = avatar.name;
-
-        // Add cloned avatar and name span to list item
-        listItemFlexbox.append(clonedFocusableAvatar, nameSpan);
-        listItem.appendChild(listItemFlexbox);
-      }
-    }
-
-    // Create click listener to pass the click on the list item down to the button or link
-    const clickListener = (event: Event) => {
-      event.preventDefault();
-      event.stopPropagation();
-
-      if (buttonOrLink instanceof HTMLElement) {
-        buttonOrLink.click();
-      }
-    };
-
-    // Add click event listener to the list item
-    listItem.addEventListener('click', clickListener);
-
-    // Store the listener reference
-    this.listItemClickListeners.set(listItem, clickListener);
-
-    // Append the list item to the list
-    if (list) {
-      writeTask(() => {
-        list.appendChild(listItem);
-      });
+  private renderAvatar(
+    avatar: ProcessedAvatar,
+    avatarSize: GuxAvatarSize
+  ): JSX.Element {
+    if (avatar.button) {
+      return (
+        <button type={avatar.button.type} onClick={avatar.button.onclick}>
+          <gux-avatar-beta
+            size={avatarSize}
+            name={avatar.name}
+            accent={avatar.accent}
+          >
+            {avatar.img ? (
+              <img slot="image" src={avatar.img.src} alt={avatar.img.alt} />
+            ) : null}
+          </gux-avatar-beta>
+        </button>
+      ) as JSX.Element;
+    } else if (avatar.link) {
+      return (
+        <a href={avatar.link.href} target={avatar.link.target}>
+          <gux-avatar-beta
+            size={avatarSize}
+            name={avatar.name}
+            accent={avatar.accent}
+          >
+            {avatar.img ? (
+              <img slot="image" src={avatar.img.src} alt={avatar.img.alt} />
+            ) : null}
+          </gux-avatar-beta>
+        </a>
+      ) as JSX.Element;
+    } else {
+      return (
+        <gux-avatar-beta
+          size={avatarSize}
+          name={avatar.name}
+          accent={avatar.accent}
+        >
+          {avatar.img ? (
+            <img slot="image" src={avatar.img.src} alt={avatar.img.alt} />
+          ) : null}
+        </gux-avatar-beta>
+      ) as JSX.Element;
     }
   }
 
-  private removeItemFromPopup(
-    focusableAvatar: HTMLGuxAvatarFocusableBetaElement
-  ): void {
-    const listItem = Array.from(
-      this.root.shadowRoot.querySelectorAll('gux-list-item')
-    ).find(item => item.id === focusableAvatar.id);
+  private renderListItem(avatar: ProcessedAvatar): JSX.Element {
+    const handleClick = (event: MouseEvent) =>
+      this.handleListItemClick(event, avatar);
+    this.listItemClickListeners.set(avatar.focusableAvatar, handleClick);
 
-    if (listItem) {
-      writeTask(() => {
-        listItem.remove();
-      });
+    return (
+      <gux-list-item key={avatar.name} onClick={handleClick}>
+        <div class="gux-overflow-list-item">
+          {this.renderAvatar(avatar, 'xsmall')}
+          {avatar.name}
+        </div>
+      </gux-list-item>
+    ) as JSX.Element;
+  }
+
+  private handleListItemClick(
+    event: MouseEvent,
+    avatar: ProcessedAvatar
+  ): void {
+    if (avatar.button) {
+      event.preventDefault();
+      avatar.button.click();
+    } else if (avatar.link) {
+      event.preventDefault();
+      avatar.link.click();
     }
   }
 
   private removeAllListeners() {
-    this.listItemClickListeners.forEach((listener, listItem) => {
-      listItem.removeEventListener('click', listener);
+    this.listItemClickListeners.forEach((listener, element) => {
+      element.removeEventListener('click', listener);
     });
     this.listItemClickListeners.clear();
   }
 
-  private slotChanged(): void {
-    this.avatarList = Array.from(
-      this.root.children
-    ) as HTMLGuxAvatarFocusableBetaElement[];
-
-    this.avatarList.forEach((focusableAvatar, index) => {
-      const avatar = focusableAvatar.querySelector(
-        'gux-avatar-beta'
-      ) as HTMLGuxAvatarBetaElement;
-
-      if (avatar) {
-        avatar.accent = this.getAccent(index) as GuxAvatarAccent;
-      }
-    });
-  }
-
   render(): JSX.Element {
+    const visibleAvatars = this.processedAvatars.filter(
+      avatar => !avatar.isOverflow
+    );
+    const overflowAvatars = this.processedAvatars.filter(
+      avatar => avatar.isOverflow
+    );
+
     return (
       <div class="gux-avatar-group-wrapper">
         <div class="gux-avatar-group">
-          <slot onSlotchange={this.slotChanged.bind(this)} />
-          <gux-avatar-overflow-beta
-            class={{ 'gux-overflow-hide': this.overflowCount === 0 }}
-            count={this.overflowCount}
-          >
-            <gux-list></gux-list>
-          </gux-avatar-overflow-beta>
+          {visibleAvatars.map(avatar => (
+            <gux-avatar-focusable-beta>
+              {this.renderAvatar(avatar, 'small')}
+            </gux-avatar-focusable-beta>
+          ))}
+          {overflowAvatars.length > 0 && (
+            <gux-avatar-overflow-beta count={overflowAvatars.length}>
+              <gux-list>
+                {overflowAvatars.map(avatar => this.renderListItem(avatar))}
+              </gux-list>
+            </gux-avatar-overflow-beta>
+          )}
         </div>
       </div>
     ) as JSX.Element;
