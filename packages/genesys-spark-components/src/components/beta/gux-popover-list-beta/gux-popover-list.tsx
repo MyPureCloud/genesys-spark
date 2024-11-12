451ca19f009ea8c98933765bc88cd413f30cfdab
jason-evans-genesys <127438502+jason-evans-genesys@users.noreply.github.com>
2024-11-12 11:54:09 -0500

    chore(popover-list-beta): Added popover-list-beta component
    ✅ Closes: COMUI-3184
    
    chore(popover-list-beta): Fixed failing tests
    
    ✅ Closes: COMUI-3184
    
    chore(popover-list-beta): Added TODO for arrow visibility
    
    ✅ Closes: COMUI-3184
    
    test(popover-list-beta): Fixed test
    
    ✅ Closes: COMUI-3184
    
    test(popover-list-beta): Fixed snapshot
    
    ✅ Closes: COMUI-3184
    
    test(popover-list-beta): Fixed snapshot
    
    ✅ Closes: COMUI-3184
    
    chore(popover-list-beta): Reduced offset value
    
    ✅ Closes: COMUI-3184

---
+++ packages/genesys-spark-components/src/components/beta/gux-popover-list-beta/gux-popover-list.tsx
@@ -0,0 +1,246 @@
+import {
+  Component,
+  Element,
+  Event,
+  EventEmitter,
+  h,
+  JSX,
+  Listen,
+  Prop
+} from '@stencil/core';
+import {
+  autoUpdate,
+  computePosition,
+  arrow,
+  flip,
+  offset,
+  Placement,
+  shift,
+  hide
+} from '@floating-ui/dom';
+
+import { OnClickOutside } from '@utils/decorator/on-click-outside';
+import { trackComponent } from '@utils/tracking/usage';
+import { findElementById } from '@utils/dom/find-element-by-id';
+
+/**
+ * @slot - popover content
+ */
+
+@Component({
+  styleUrl: 'gux-popover-list.scss',
+  tag: 'gux-popover-list-beta',
+  shadow: true
+})
+export class GuxPopoverList {
+  private popupElement: HTMLDivElement;
+  private arrowElement: HTMLDivElement;
+  private cleanupUpdatePosition: ReturnType<typeof autoUpdate>;
+
+  @Element()
+  private root: HTMLElement;
+
+  /**
+   * Indicates the id of the element the popover should anchor to
+   */
+  @Prop()
+  for: string;
+
+  /**
+   * Indicate position of popover element arrow (follow floating ui placement attribute api)
+   */
+  @Prop()
+  position: Placement = 'bottom';
+
+  /**
+   * Indicate if the dismiss button is displayed
+   */
+  @Prop()
+  displayDismissButton: boolean;
+
+  /**
+   * Close popover when the user clicks outside of its bounds
+   */
+  @Prop()
+  closeOnClickOutside: boolean = false;
+
+  /**
+   * Controls hiding and showing the popover
+   */
+  @Prop({ mutable: true })
+  isOpen: boolean = false;
+
+  /**
+   * Fired when a user dismisses the popover
+   */
+  @Event()
+  guxdismiss: EventEmitter<void>;
+
+  @Listen('keydown')
+  onKeyDown(event: KeyboardEvent): void {
+    switch (event.key) {
+      case 'Tab':
+      case 'Escape':
+        this.dismiss();
+        break;
+    }
+  }
+
+  @OnClickOutside({ triggerEvents: 'mousedown' })
+  checkForClickOutside(event: MouseEvent) {
+    const clickPath = event.composedPath();
+    const forElement = findElementById(this.root, this.for);
+    const clickedForElement = clickPath.includes(forElement);
+
+    if (
+      (this.closeOnClickOutside || !this.displayDismissButton) &&
+      this.isOpen &&
+      !clickedForElement
+    ) {
+      this.dismiss();
+    }
+
+    if (clickedForElement) {
+      this.popupElement.togglePopover();
+      this.isOpen = !this.isOpen;
+      this.runUpdatePosition();
+    }
+  }
+
+  private runUpdatePosition(): void {
+    if (this.root.isConnected) {
+      this.cleanupUpdatePosition = autoUpdate(
+        findElementById(this.root, this.for),
+        this.popupElement,
+        () => this.updatePosition(),
+        {
+          ancestorScroll: true,
+          elementResize: true,
+          animationFrame: true,
+          ancestorResize: true
+        }
+      );
+    } else {
+      this.disconnectedCallback();
+    }
+  }
+
+  private updatePosition(): void {
+    const forElement = findElementById(this.root, this.for);
+    // This is 13 because this makes the arrow look aligned
+    const arrowLen = 13;
+
+    if (this.popupElement) {
+      void computePosition(forElement, this.popupElement, {
+        strategy: 'fixed',
+        placement: this.position,
+        middleware: [
+          offset(2),
+          flip(),
+          shift(),
+          hide(),
+          arrow({
+            element: this.arrowElement,
+            padding: 16
+          })
+        ]
+      }).then(({ x, y, middlewareData, placement }) => {
+        Object.assign(this.popupElement.style, {
+          left: `${x}px`,
+          top: `${y}px`
+        });
+
+        const side = placement.split('-')[0];
+
+        const staticSide = {
+          top: 'bottom',
+          right: 'left',
+          bottom: 'top',
+          left: 'right'
+        }[side];
+
+        // TODO: COMUI-3210 - Arrow currently does not show
+        if (middlewareData.arrow) {
+          const { x, y } = middlewareData.arrow;
+          this.popupElement.setAttribute('data-placement', placement);
+          Object.assign(this.arrowElement.style, {
+            left: x != null ? `${x}px` : '',
+            top: y != null ? `${y}px` : '',
+            right: '',
+            bottom: '',
+            [staticSide]: `${-arrowLen / 2}px`,
+            transform: 'rotate(45deg)'
+          });
+        }
+
+        if (middlewareData.hide) {
+          Object.assign(this.popupElement.style, {
+            visibility: middlewareData.hide.referenceHidden
+              ? 'hidden'
+              : 'visible'
+          });
+        }
+      });
+    }
+  }
+
+  private dismiss(): void {
+    const dismissEvent = this.guxdismiss.emit();
+    if (!dismissEvent.defaultPrevented) {
+      this.isOpen = false;
+      this.popupElement.togglePopover();
+    }
+  }
+
+  connectedCallback(): void {
+    trackComponent(this.root, { variant: this.position });
+  }
+
+  componentDidLoad(): void {
+    if (this.isOpen) {
+      this.popupElement.togglePopover();
+      this.runUpdatePosition();
+    }
+  }
+
+  componentDidUpdate(): void {
+    if (this.isOpen) {
+      this.runUpdatePosition();
+    } else if (this.cleanupUpdatePosition) {
+      this.cleanupUpdatePosition();
+    }
+  }
+
+  disconnectedCallback(): void {
+    if (this.cleanupUpdatePosition) {
+      this.cleanupUpdatePosition();
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <div
+        ref={(el: HTMLDivElement) => (this.popupElement = el)}
+        class={{
+          'gux-hidden': !this.isOpen,
+          'gux-popover-wrapper': true
+        }}
+        data-placement
+        popover="manual"
+      >
+        <div
+          ref={(el: HTMLDivElement) => (this.arrowElement = el)}
+          class="gux-arrow"
+        ></div>
+        {this.displayDismissButton && (
+          <gux-dismiss-button
+            onClick={this.dismiss.bind(this)}
+          ></gux-dismiss-button>
+        )}
+        <div class="gux-popover-content">
+          <slot />
+        </div>
+      </div>
+    ) as JSX.Element;
+  }
+}
--- packages/genesys-spark-components/src/components/stable/gux-dismiss-button/readme.md
+++ packages/genesys-spark-components/src/components/stable/gux-dismiss-button/readme.md
@@ -23,6 +23,7 @@ This component is meant for use in other components as a dismiss button.
  - [gux-popover](../gux-popover)
  - [gux-popover-beta](../../beta/gux-popover-beta)
  - [gux-popover-list](../gux-popover-list)
+ - [gux-popover-list-beta](../../beta/gux-popover-list-beta)
  - [gux-simple-toast-legacy](../../legacy/gux-simple-toast-legacy)
  - [gux-toast](../gux-toast)
 
@@ -42,6 +43,7 @@ graph TD;
   gux-popover --> gux-dismiss-button
   gux-popover-beta --> gux-dismiss-button
   gux-popover-list --> gux-dismiss-button
+  gux-popover-list-beta --> gux-dismiss-button
   gux-simple-toast-legacy --> gux-dismiss-button
   gux-toast --> gux-dismiss-button
   style gux-dismiss-button fill:#f9f,stroke:#333,stroke-width:4px
