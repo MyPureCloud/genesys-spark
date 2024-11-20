4bfabc6ed6e2967c762b6ca96f4451b4b8331618
321gillian <gillian.middleton@genesys.com>
2024-11-20 17:43:27 +0000

    fix(label-info): Pass formatting through to tooltip (#703)
    * fix(label-info): Pass formatting through to tooltip
    
    Keep tooltip formatting in label-info
    
    ✅ Closes: COMUI-3227
    
    * chore(other): redo tooltip handling

--- packages/genesys-spark-components/src/components/beta/gux-label-info/gux-label-info.tsx
+++ packages/genesys-spark-components/src/components/beta/gux-label-info/gux-label-info.tsx
@@ -4,7 +4,7 @@ import { Placement } from '@floating-ui/dom';
 
 import { trackComponent } from '@utils/tracking/usage';
 import { GuxLabelInfoVariant } from './gux-label-info.types';
-import { getSlotTextContent } from '../../../utils/dom/get-slot-text-content';
+import { getSlotTextContent } from '@utils/dom/get-slot-text-content';
 
 /**
  * @slot content - Required slot for tooltip and screenreader content
@@ -57,7 +57,7 @@ export class GuxLabelInfo {
     return (
       <div class="gux-label-info">
         <gux-screen-reader-beta>
-          <slot name="content"></slot>
+          {getSlotTextContent(this.root, 'content')}
         </gux-screen-reader-beta>
         <gux-icon
           icon-name={this.getVariantIcon(this.variant)}
@@ -68,7 +68,7 @@ export class GuxLabelInfo {
           placement={this.placement}
           ref={el => (this.tooltipElement = el)}
         >
-          <div slot="content">{getSlotTextContent(this.root, 'content')}</div>
+          <slot name="content"></slot>
         </gux-tooltip-beta>
       </div>
     ) as JSX.Element;
