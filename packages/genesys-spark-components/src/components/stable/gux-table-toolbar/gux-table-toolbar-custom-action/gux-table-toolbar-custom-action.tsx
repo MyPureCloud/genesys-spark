a1967e461d16bb8fba44ef025589f99d8a212da7
Daragh King <daragh.king@genesys.com>
2024-11-22 14:42:20 +0000

    fix(table-toolbar): always show label when in the condensed display
    always show label when in the condensed display
    
    ✅ Closes: COMUI-3213

--- packages/genesys-spark-components/src/components/stable/gux-table-toolbar/gux-table-toolbar-custom-action/gux-table-toolbar-custom-action.tsx
+++ packages/genesys-spark-components/src/components/stable/gux-table-toolbar/gux-table-toolbar-custom-action/gux-table-toolbar-custom-action.tsx
@@ -1,4 +1,6 @@
 import { Component, Element, JSX, h, Listen, Prop } from '@stencil/core';
+import { getClosestElement } from 'genesys-spark-utils/get-closest-element';
+
 import { trackComponent } from '@utils/tracking/usage';
 import { GuxTableToolbarActionAccent } from '../gux-table-toolbar-action-accents.types';
 import { getSlotTextContent } from '@utils/dom/get-slot-text-content';
@@ -33,8 +35,19 @@ export class GuxTableToolbarCustomAction {
     }
   }
 
+  private hideText(): boolean {
+    const parent = getClosestElement(
+      this.root,
+      'gux-table-toolbar'
+    ) as HTMLGuxTableToolbarElement;
+    const nonCondensedParentLayout =
+      parent?.getAttribute('gs-layout') !== 'condensed';
+
+    return this.iconOnly && nonCondensedParentLayout;
+  }
+
   private renderTooltip(): JSX.Element {
-    if (this.iconOnly) {
+    if (this.hideText()) {
       return (
         <gux-tooltip>
           <div slot="content">{getSlotTextContent(this.root, 'text')}</div>
@@ -52,7 +65,7 @@ export class GuxTableToolbarCustomAction {
       <gux-button-slot accent={this.accent}>
         <button disabled={this.disabled} type="button" class="gux-action-title">
           <slot name="icon" />
-          <span class={{ 'gux-sr-only': this.iconOnly }}>
+          <span class={{ 'gux-sr-only': this.hideText() }}>
             <slot name="text" />
           </span>
         </button>
