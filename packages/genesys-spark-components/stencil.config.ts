a3fe69110dfd06cfd062fe7ec6b2e65ceb90e3c5
Jason Mobley <jason.mobley@genesys.com>
2024-12-03 14:05:34 -0500

    feat: override CDN URL via assetsUrl option to registerSparkComponents

--- packages/genesys-spark-components/stencil.config.ts
+++ packages/genesys-spark-components/stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { Credentials } from '@stencil/core/internal';
 import { sass } from '@stencil/sass';
 import copy from 'rollup-plugin-copy';
 import generateMetadata from './scripts/generate-component-data';
@@ -8,6 +9,16 @@ const testConsoleReporter =
   process.env.DEFAULT_TEST_REPORTER === 'true'
     ? 'default'
     : ['jest-silent-reporter', { useDots: true }];
+
+// Optionally host the dev server with https if a cert and key are provided via env variables, e.g.
+// for running local dev build within a https app using assetsUrl option of registerSparkComponents.
+let https: Credentials | undefined = undefined;
+if (process.env.SPARK_HTTPS_CERT && process.env.SPARK_HTTPS_KEY) {
+  https = {
+    key: process.env.SPARK_HTTPS_KEY,
+    cert: process.env.SPARK_HTTPS_CERT
+  };
+}
 
 export const config: Config = {
   namespace: 'genesys-webcomponents',
@@ -81,6 +92,7 @@ export const config: Config = {
     experimentalImportInjection: true
   },
   devServer: {
-    port: 3733
+    port: 3733,
+    https
   }
 };
