ccc246c0c219d7f8940948dff62bf5bcaf9e6792
321gillian <gillian.middleton@gmail.com>
2025-01-13 10:02:52 +0000

    chore(rich-text-editor): fix e2e test

--- packages/genesys-spark-components/src/components/beta/gux-rich-text-editor/gux-rich-text-editor-action/gux-rich-text-editor-action-link/tests/gux-rich-text-editor-action-link.e2e.ts
+++ packages/genesys-spark-components/src/components/beta/gux-rich-text-editor/gux-rich-text-editor-action/gux-rich-text-editor-action-link/tests/gux-rich-text-editor-action-link.e2e.ts
@@ -1,7 +1,26 @@
+import { E2EPage, newE2EPage } from '@stencil/core/testing';
 import {
   newSparkE2EPage,
   a11yCheck
 } from '../../../../../../test/e2eTestUtils';
+
+const axeExclusions = [];
+
+async function newNonrandomE2EPage({
+  html
+}: {
+  html: string;
+}): Promise<E2EPage> {
+  const page = await newE2EPage();
+
+  await page.evaluateOnNewDocument(() => {
+    Math.random = () => 0.5;
+  });
+  await page.setContent(html);
+  await page.waitForChanges();
+
+  return page;
+}
 
 describe('gux-rich-text-editor-action-link', () => {
   describe('#render', () => {
@@ -14,13 +33,16 @@ describe('gux-rich-text-editor-action-link', () => {
     </gux-rich-text-editor-beta>`
     ].forEach((html, index) => {
       it(`should display component as expected (${index + 1})`, async () => {
-        const page = await newSparkE2EPage({ html });
+        const page = await newNonrandomE2EPage({ html });
 
         const element = await page.find('gux-rich-text-editor-action-link');
-
-        await a11yCheck(page);
-
         expect(element.outerHTML).toMatchSnapshot();
+      });
+
+      it(`should be accessible (${index + 1})`, async () => {
+        const page = await newSparkE2EPage({ html });
+
+        await a11yCheck(page, axeExclusions);
       });
     });
   });
