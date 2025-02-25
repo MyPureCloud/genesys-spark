0c7a90b5c621dd07fecc2c35303d87ee63bb4951
Daragh King <daragh.king@genesys.com>
2025-02-25 21:35:19 +0000

    feat(other): added less and sass tokens
    added less and sass tokens
    
    ✅ Closes: COMUI-3286

--- packages/genesys-spark-tokens/test/index.mjs
+++ packages/genesys-spark-tokens/test/index.mjs
@@ -4,21 +4,27 @@ import dircompare from 'dir-compare';
 import { getRootPath } from '../utils/get-root-path.mjs';
 
 const options = { compareContent: true };
-
-const dist = path.resolve(getRootPath(), 'dist');
-const snapshot = path.resolve(getRootPath(), 'snapshot');
+const rootPath = getRootPath();
+const dist = path.resolve(rootPath, 'dist');
+const snapshot = path.resolve(rootPath, 'snapshot');
 
 try {
   const result = dircompare.compareSync(dist, snapshot, options);
 
   if (!result.same) {
-    console.warn(`❌  'dist' and 'snapshot' folders are different.\n`);
+    console.warn(
+      `${rootPath}: ❌  'dist' and 'snapshot' folders are different.\n`
+    );
     console.error(result);
     process.exit(1);
   }
 
-  console.info(`✅  'dist' and 'snapshot' folders are identical.\n`);
+  console.info(
+    `${rootPath}: ✅  'dist' and 'snapshot' folders are identical.\n`
+  );
 } catch {
-  console.warn(`❌  'dist' folder missing. Run 'npm run build'.\n`);
+  console.warn(
+    `${rootPath}: ❌  'dist' folder missing. Run 'npm run build'.\n`
+  );
   process.exit(1);
 }
