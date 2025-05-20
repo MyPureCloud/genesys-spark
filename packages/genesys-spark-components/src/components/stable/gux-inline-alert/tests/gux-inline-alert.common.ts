a15ffcc6a0dd7f71aefa500356d775cb9e653a36
321gillian <gillian.middleton@genesys.com>
2025-05-20 17:37:22 +0100

    test(inline-alert): Playwright tests (#1098)
    ✅ Closes: COMUI-3778

---
+++ packages/genesys-spark-components/src/components/stable/gux-inline-alert/tests/gux-inline-alert.common.ts
@@ -0,0 +1,18 @@
+export const renderConfigs = [
+  {
+    description: '"info" accent inline alert',
+    html: `<gux-inline-alert accent="info"><div slot="content">Note: This is an information alert.</div></gux-inline-alert>`
+  },
+  {
+    description: '"success" accent inline alert',
+    html: `<gux-inline-alert accent="success"><div slot="content">Note: This is a success alert.</div></gux-inline-alert>`
+  },
+  {
+    description: '"warning" accent inline alert',
+    html: `<gux-inline-alert accent="warning"><div slot="content">Note: This is a warning alert.</div></gux-inline-alert>`
+  },
+  {
+    description: '"error" accent inline alert',
+    html: `<gux-inline-alert accent="error"><div slot="content">Note: This is a warning alert.</div></gux-inline-alert>`
+  }
+];
--- packages/genesys-spark-components/src/components/stable/gux-inline-alert/tests/gux-inline-alert.spec.ts
+++ packages/genesys-spark-components/src/components/stable/gux-inline-alert/tests/gux-inline-alert.spec.ts
@@ -1,22 +1,11 @@
-import { newSpecPage } from '@test/specTestUtils';
+import { checkRenders } from '@test/specTestUtils';
 import { GuxAlert } from '../gux-inline-alert';
+import { renderConfigs } from './gux-inline-alert.common';
 
 const components = [GuxAlert];
-const language = 'en';
 
 describe('gux-inline-alert', () => {
   describe('#render', () => {
-    [
-      `<gux-inline-alert accent="info"><div slot="content">Note: This is an information alert.</div></gux-inline-alert>`,
-      `<gux-inline-alert accent="success"><div slot="content">Note: This is a success alert.</div></gux-inline-alert>`,
-      `<gux-inline-alert accent="warning"><div slot="content">Note: This is a warning alert.</div></gux-inline-alert>`,
-      `<gux-inline-alert accent="error"><div slot="content">Note: This is a warning alert.</div></gux-inline-alert>`
-    ].forEach((html, index) => {
-      it(`should render component as expected (${index + 1})`, async () => {
-        const page = await newSpecPage({ components, html, language });
-
-        expect(page.root).toMatchSnapshot();
-      });
-    });
+    checkRenders(renderConfigs, components);
   });
 });
