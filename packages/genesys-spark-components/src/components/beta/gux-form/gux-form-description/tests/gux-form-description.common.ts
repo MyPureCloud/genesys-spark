7dd5f0c576a03f447cc9a28450113195ffb07953
jason-evans-genesys <127438502+jason-evans-genesys@users.noreply.github.com>
2025-05-20 07:21:29 -0700

    chore(form-description): Fixed test html text
    ✅ Closes: COMUI-3776

--- packages/genesys-spark-components/src/components/beta/gux-form/gux-form-description/tests/gux-form-description.common.ts
+++ packages/genesys-spark-components/src/components/beta/gux-form/gux-form-description/tests/gux-form-description.common.ts
@@ -1,3 +1,3 @@
 export const renderConfig = {
-  html: `<gux-form-description></gux-form-description>`
+  html: `<gux-form-description><p slot="description">This is a simple paragraph to accompany the form header.</p></gux-form-description>`
 };
