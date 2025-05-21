be679f02745559094516b1efd19504aa24e30674
System Administrator <gavin.everett@genesys.com>
2025-05-21 16:26:34 +0100

    test(card): update playwright tests
    update playwright tests
    
    ✅ Closes: COMUI-3773

--- packages/genesys-spark-components/src/components/stable/gux-card/tests/gux-card.e2e.playwright.ts
+++ packages/genesys-spark-components/src/components/stable/gux-card/tests/gux-card.e2e.playwright.ts
@@ -2,5 +2,5 @@ import { checkRenders, test } from '@test/playwrightTestUtils';
 import { renderConfigs } from './gux-card.common';
 
 test.describe('gux-card', () => {
-  checkRenders(renderConfigs, 'gux-card');
+  checkRenders(renderConfigs);
 });
