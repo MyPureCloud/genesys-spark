17f9397ccd1093f8880c800910bb13eaffefc74e
Daragh King <daragh.king@genesys.com>
2025-05-22 13:12:01 +0100

    test(date-time-beta): added Playwright tests
    added Playwright tests
    
    ✅ Closes: COMUI-3784

---
+++ packages/genesys-spark-components/src/components/beta/gux-date-time/tests/gux-date-time.e2e.playwright.ts
@@ -0,0 +1,6 @@
+import { checkRenders, test } from '@test/playwrightTestUtils';
+import { renderConfigs } from './gux-date-time.common';
+
+test.describe('gux-date-time', () => {
+  checkRenders(renderConfigs, 'gux-date-time-beta');
+});
--- packages/genesys-spark-components/src/components/beta/gux-date-time/tests/gux-date-time.spec.ts
+++ packages/genesys-spark-components/src/components/beta/gux-date-time/tests/gux-date-time.spec.ts
@@ -5,73 +5,53 @@ jest.mock('i18n/use-regional-dates', () => ({
   useRegionalDates
 }));
 
-import { newSpecPage } from '@test/specTestUtils';
+import { checkRenders } from '@test/specTestUtils';
 import { GuxDateTime } from '../gux-date-time';
-import { timeZoneIdentifiers } from '../../../../i18n/time-zone/identifiers';
 import { languageList } from '../../../../i18n/languageList';
+import { renderConfigs, timezoneRenderConfigs } from './gux-date-time.common';
 
 const components = [GuxDateTime];
-const language = 'en';
-const date = '2022-07-07T13:35:30.100Z';
-
-const testCases = [
-  `<gux-date-time-beta datetime=${date} format="short"></gux-date-time-beta>`,
-  `<gux-date-time-beta datetime=${date} format="medium"></gux-date-time-beta>`,
-  `<gux-date-time-beta datetime=${date} format="full"></gux-date-time-beta>`,
-  `<gux-date-time-beta datetime=${date} format="long"></gux-date-time-beta>`
-];
 
 describe('gux-date-time', () => {
-  describe('#render using non regional dates', () => {
+  describe('#render regional dates', () => {
     beforeAll(() => {
-      useRegionalDates.mockReturnValue(false);
+      useRegionalDates.mockReturnValue(true);
     });
-    describe('#render different datetime formats eg(short,medium,full,long)', () => {
-      languageList.forEach(language => {
-        testCases.forEach((html, index) => {
-          it(`should render component as expected (${index + 1})`, async () => {
-            const page = await newSpecPage({ components, html, language });
 
-            expect(page.root).toMatchSnapshot();
-          });
-        });
-      });
+    describe('#render', () => {
+      checkRenders(renderConfigs, components);
     });
 
     describe('#render different time zones', () => {
-      timeZoneIdentifiers.forEach((timeZone: string) => {
-        it(`should work as expected for "${timeZone}"`, async () => {
-          const html = `<gux-date-time-beta time-zone=${timeZone} datetime="2022-07-07T12:00:00.000Z" format="full"></gux-date-time-beta>`;
-          const page = await newSpecPage({ components, html, language });
+      checkRenders(timezoneRenderConfigs, components);
+    });
 
-          expect(page.root).toMatchSnapshot();
+    describe('#render different languages', () => {
+      languageList.forEach(language => {
+        describe(`#${language}`, () => {
+          checkRenders(renderConfigs, components, language);
         });
       });
     });
   });
-  describe('#render using regional dates', () => {
+
+  describe('#render using non regional dates', () => {
     beforeAll(() => {
       useRegionalDates.mockReturnValue(true);
     });
-    describe('#render different datetime formats eg(short,medium,full,long)', () => {
-      languageList.forEach(language => {
-        testCases.forEach((html, index) => {
-          it(`should render component as expected (${index + 1})`, async () => {
-            const page = await newSpecPage({ components, html, language });
 
-            expect(page.root).toMatchSnapshot();
-          });
-        });
-      });
+    describe('#render', () => {
+      checkRenders(renderConfigs, components);
     });
 
     describe('#render different time zones', () => {
-      timeZoneIdentifiers.forEach((timeZone: string) => {
-        it(`should work as expected for "${timeZone}"`, async () => {
-          const html = `<gux-date-time-beta time-zone=${timeZone} datetime="2022-07-07T12:00:00.000Z" format="full"></gux-date-time-beta>`;
-          const page = await newSpecPage({ components, html, language });
+      checkRenders(timezoneRenderConfigs, components);
+    });
 
-          expect(page.root).toMatchSnapshot();
+    describe('#render different languages', () => {
+      languageList.forEach(language => {
+        describe(`#${language}`, () => {
+          checkRenders(renderConfigs, components, language);
         });
       });
     });
