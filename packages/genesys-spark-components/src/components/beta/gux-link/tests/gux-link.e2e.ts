d0aa886a8b16576f5ee0a16823c7dd003b86f3cf
321gillian <gillian.middleton@gmail.com>
2025-05-28 12:43:41 +0100

    test(link-beta): Add playwright tests
    ✅ Closes: COMUI-3777

--- packages/genesys-spark-components/src/components/beta/gux-link/tests/gux-link.e2e.ts
+++ packages/genesys-spark-components/src/components/beta/gux-link/tests/gux-link.e2e.ts
@@ -1,15 +1,11 @@
 import { newSparkE2EPage, a11yCheck } from '@test/e2eTestUtils';
+import { renderConfigs } from './gux-link.common';
+
 describe('gux-link-beta', () => {
   describe('#render', () => {
-    [
-      `<gux-link-beta><a href="#">Link</a></gux-link-beta>`,
-      `<gux-link-beta size="small"><a href="#">Link</a></gux-link-beta>`,
-      `<gux-link-beta standalone><a href="#">Link</a></gux-link-beta>`,
-      `<gux-link-beta size="small" standalone><a href="#">Link</a></gux-link-beta>`,
-      `<gux-link-beta standalone><a href="#">Link<gux-icon icon-name="external-link" decorative></a></gux-icon></gux-link-beta>`
-    ].forEach((html, index) => {
-      it(`should render component as expected (${index + 1})`, async () => {
-        const page = await newSparkE2EPage({ html });
+    renderConfigs.forEach(config => {
+      it(config.description, async () => {
+        const page = await newSparkE2EPage({ html: config.html });
         await a11yCheck(page);
 
         const element = await page.find('gux-link-beta');
