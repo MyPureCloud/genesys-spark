09d26b36fa7055a8d0ff967ab3354cc0283a20e7
jason-evans-genesys <127438502+jason-evans-genesys@users.noreply.github.com>
2025-06-04 18:34:46 +0200

    chore(flyout-menu): Reversed accidental flyout-menu playwright code
    ✅ Closes: COMUI-826

--- packages/genesys-spark-components/src/components/stable/gux-popover-list/tests/gux-popover-list.e2e.playwright.ts
+++
@@ -1,6 +0,0 @@
-import { checkRenders, test } from '@test/playwrightTestUtils';
-import { renderConfig } from './gux-popover-list.common';
-
-test.describe('gux-popover-list', () => {
-  checkRenders([renderConfig], 'gux-popover-list');
-});
--- packages/genesys-spark-components/src/components/stable/gux-popover-list/tests/gux-popover-list.e2e.ts
+++ packages/genesys-spark-components/src/components/stable/gux-popover-list/tests/gux-popover-list.e2e.ts
@@ -1,11 +1,24 @@
 import { newSparkE2EPage, a11yCheck } from '../../../../test/e2eTestUtils';
-import { renderConfig } from './gux-popover-list.common';
 
 describe('gux-popover-list', () => {
-  it(renderConfig.description, async () => {
+  it('renders', async () => {
     const page = await newSparkE2EPage({
-      html: renderConfig.html
+      html: `
+      <div lang="en">
+        <div id="popover-target">
+          Example Element
+        </div>
+        <gux-popover-list position="top" for="popover-target" is-open>
+          <gux-list>
+            <gux-list-item>Item 1</gux-list-item>
+            <gux-list-item>Item 2</gux-list-item>
+            <gux-list-item>Item 3</gux-list-item>
+          </gux-list>
+        </gux-popover-list>
+      </div>
+      `
     });
+
     const element = await page.find('gux-popover-list');
     await a11yCheck(page);
     expect(element).toHaveAttribute('hydrated');
