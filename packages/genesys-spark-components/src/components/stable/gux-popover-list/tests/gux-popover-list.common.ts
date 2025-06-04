5d5e5ee60fb3825e5a2a21f301363879ec715215
jason-evans-genesys <127438502+jason-evans-genesys@users.noreply.github.com>
2025-06-04 18:32:15 +0200

    chore(popover-list): Added playwright tests
    ✅ Closes: COMUI-3786

---
+++ packages/genesys-spark-components/src/components/stable/gux-popover-list/tests/gux-popover-list.common.ts
@@ -0,0 +1,15 @@
+export const renderConfig = {
+  description: 'should render gux-popover-list',
+  html: `<div lang="en">
+        <div id="popover-target">
+          Example Element
+        </div>
+        <gux-popover-list position="top" for="popover-target" is-open>
+          <gux-list>
+            <gux-list-item>Item 1</gux-list-item>
+            <gux-list-item>Item 2</gux-list-item>
+            <gux-list-item>Item 3</gux-list-item>
+          </gux-list>
+        </gux-popover-list>
+      </div>`
+};
--- packages/genesys-spark-components/src/components/stable/gux-popover-list/tests/gux-popover-list.e2e.ts
+++ packages/genesys-spark-components/src/components/stable/gux-popover-list/tests/gux-popover-list.e2e.ts
@@ -1,24 +1,11 @@
 import { newSparkE2EPage, a11yCheck } from '../../../../test/e2eTestUtils';
+import { renderConfig } from './gux-popover-list.common';
 
 describe('gux-popover-list', () => {
-  it('renders', async () => {
+  it(renderConfig.description, async () => {
     const page = await newSparkE2EPage({
-      html: `
-      <div lang="en">
-        <div id="popover-target">
-          Example Element
-        </div>
-        <gux-popover-list position="top" for="popover-target" is-open>
-          <gux-list>
-            <gux-list-item>Item 1</gux-list-item>
-            <gux-list-item>Item 2</gux-list-item>
-            <gux-list-item>Item 3</gux-list-item>
-          </gux-list>
-        </gux-popover-list>
-      </div>
-      `
+      html: renderConfig.html
     });
-
     const element = await page.find('gux-popover-list');
     await a11yCheck(page);
     expect(element).toHaveAttribute('hydrated');
