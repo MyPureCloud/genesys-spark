2aa45c869d6d5f7345e82a460888cc352884ebb0
System Administrator <gavin.everett@genesys.com>
2025-06-11 10:30:35 +0100

    test(stepper-beta): update playwright tests
    update playwright tests
    
    ✅ Closes: COMUI-3739

--- packages/genesys-spark-components/src/components/beta/gux-stepper/tests/gux-stepper.common.ts
+++ packages/genesys-spark-components/src/components/beta/gux-stepper/tests/gux-stepper.common.ts
@@ -22,7 +22,7 @@ export const renderConfigs = [
     `
   },
   {
-    description: 'Should render as expected for vertical state',
+    description: 'Should render as expected for vertical orientation',
     html: `
         <gux-stepper-beta orientation="vertical">
         <gux-step step-id="account-info" status="incomplete">
@@ -30,6 +30,27 @@ export const renderConfigs = [
             <span slot="helper">Please enter account info</span>
         </gux-step>
         </gux-stepper-beta>
+    `
+  },
+  {
+    description: 'Should render as expected for a stepper with 4 steps',
+    html: `
+      <gux-stepper-beta active-step-id="account-info">
+        <gux-step step-id="account-info" status="completed">
+          <gux-step-title slot="title">Account Info</gux-step-title>
+          <span slot="helper">Please enter account info</span>
+        </gux-step>
+        <gux-step step-id="personal-info" status="error">
+          <gux-step-title slot="title">Personal Info</gux-step-title>
+          <span slot="helper">Please enter personal info</span>
+        </gux-step>
+        <gux-step step-id="maintenance" status="incomplete">
+          <gux-step-title slot="title">Maintenance</gux-step-title>
+        </gux-step>
+        <gux-step step-id="verification" status="incomplete">
+          <gux-step-title slot="title">Verification</gux-step-title>
+        </gux-step>
+      </gux-stepper-beta>
     `
   }
 ];
