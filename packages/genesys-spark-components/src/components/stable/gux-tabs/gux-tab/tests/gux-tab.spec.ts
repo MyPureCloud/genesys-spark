f7a24dfea0da63fd9ac74d654a6e2e98acc88c11
Daragh King <daragh.king@genesys.com>
2025-06-25 16:51:17 +0100

    chore(other): fixed spec tests
    fixed spec tests

--- packages/genesys-spark-components/src/components/stable/gux-tabs/gux-tab/tests/gux-tab.spec.ts
+++ packages/genesys-spark-components/src/components/stable/gux-tabs/gux-tab/tests/gux-tab.spec.ts
@@ -2,31 +2,33 @@ import { newSpecPage } from '@test/specTestUtils';
 import { GuxTab } from '../gux-tab';
 
 describe('gux-tab', () => {
-  let page: any;
-  let component: GuxTab;
-
-  beforeEach(async () => {
-    page = await newSpecPage({
+  it('should build', async () => {
+    const page = await newSpecPage({
       components: [GuxTab],
       html: `<gux-tab></gux-tab>`,
       language: 'en'
     });
 
-    component = page.rootInstance;
-  });
-
-  it('should build', async () => {
-    expect(component).toBeInstanceOf(GuxTab);
+    expect(page.rootInstance).toBeInstanceOf(GuxTab);
   });
 
   it('should render', async () => {
-    expect(component).toMatchSnapshot();
+    const page = await newSpecPage({
+      components: [GuxTab],
+      html: `<gux-tab></gux-tab>`,
+      language: 'en'
+    });
+
+    expect(page.root).toMatchSnapshot();
   });
 
-  // it should render disabled
   it('should render disabled', async () => {
-    page.root.setAttribute('gux-disabled', 'true');
-    await page.waitForChanges();
-    expect(component).toMatchSnapshot();
+    const page = await newSpecPage({
+      components: [GuxTab],
+      html: `<gux-tab gux-disabled="true"></gux-tab>`,
+      language: 'en'
+    });
+
+    expect(page.root).toMatchSnapshot();
   });
 });
