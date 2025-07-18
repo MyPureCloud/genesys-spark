5ebe968cb552b71c74ee73a045f8e840dfb7ec38
321gillian <gillian.middleton@gmail.com>
2025-07-18 14:56:30 +0100

    chore(table-toolbar-action): Add test for click on disabled version

--- packages/genesys-spark-components/src/components/stable/gux-table-toolbar/gux-table-toolbar-action/tests/gux-table-toolbar-action.e2e.playwright.ts
+++ packages/genesys-spark-components/src/components/stable/gux-table-toolbar/gux-table-toolbar-action/tests/gux-table-toolbar-action.e2e.playwright.ts
@@ -30,5 +30,21 @@ test.describe('gux-table-toolbar-action', () => {
 
       expect(clickSpy).toHaveReceivedEventTimes(1);
     });
+
+    test('should not fire a click event when the disabled toolbar action is clicked', async ({
+      page
+    }) => {
+      const html =
+        '<gux-table-toolbar-action action="export" disabled></gux-table-toolbar-action>';
+      await setContent(page, html);
+
+      const element = page.locator('gux-table-toolbar-action');
+      const clickSpy = await page.spyOnEvent('click');
+
+      await element.click();
+      await page.waitForChanges();
+
+      expect(clickSpy).not.toHaveReceivedEvent();
+    });
   });
 });
