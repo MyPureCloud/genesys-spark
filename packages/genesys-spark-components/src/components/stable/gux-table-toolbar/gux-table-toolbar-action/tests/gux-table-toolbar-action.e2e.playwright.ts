ff2a1c8e95bfa9be27cc64299a0b64dd040fb949
321gillian <gillian.middleton@gmail.com>
2025-07-18 12:34:39 +0100

    test(table-toolbar-action): Add playwright test
    ✅ Closes: COMUI-3875

---
+++ packages/genesys-spark-components/src/components/stable/gux-table-toolbar/gux-table-toolbar-action/tests/gux-table-toolbar-action.e2e.playwright.ts
@@ -0,0 +1,34 @@
+import {
+  checkRenders,
+  test,
+  expect,
+  setContent
+} from '@test/playwrightTestUtils';
+import { renderConfigs } from './gux-table-toolbar-action.common';
+
+test.describe('gux-table-toolbar-action', () => {
+  test.describe('#render', () => {
+    checkRenders({
+      renderConfigs,
+      element: 'gux-table-toolbar-action'
+    });
+  });
+
+  test.describe('interactions', () => {
+    test('should fire a click event when the toolbar action is clicked', async ({
+      page
+    }) => {
+      const html =
+        '<gux-table-toolbar-action action="export"></gux-table-toolbar-action>';
+      await setContent(page, html);
+
+      const element = page.locator('gux-table-toolbar-action');
+      const clickSpy = await page.spyOnEvent('click');
+
+      await element.click();
+      await page.waitForChanges();
+
+      expect(clickSpy).toHaveReceivedEventTimes(1);
+    });
+  });
+});
