f07e99d9d253b6c79cc3faf50538584634e1474d
jason-evans-genesys <127438502+jason-evans-genesys@users.noreply.github.com>
2025-07-22 13:47:34 -0700

    chore(copy-to-clipboard): Added playwright test
    ✅ Closes: COMUI-3789
    
    chore(copy-to-clipboard): Using locator
    
    ✅ Closes: COMUI-3789
    
    chore(copy-to-clipboard): Snapshot updates
    
    ✅ Closes: COMUI-3789
    
    chore(copy-to-clipboard): Fixed copy text check
    
    ✅ Closes: COMUI-3789
    
    chore(copy-to-clipboard): Using waitFor in playwright test
    
    ✅ Closes: COMUI-3789

---
+++ packages/genesys-spark-components/src/components/stable/gux-copy-to-clipboard/tests/gux-copy-to-clipboard.common.ts
@@ -0,0 +1,4 @@
+export const renderConfig = {
+  description: 'should render default control no props',
+  html: `<gux-copy-to-clipboard lang="en"><div slot="content">Test</div></gux-copy-to-clipboard>`
+};
