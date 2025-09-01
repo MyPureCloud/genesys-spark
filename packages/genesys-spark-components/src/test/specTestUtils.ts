903ba2bb77ad120faf094f8914d6809d628fc80c
Daragh King <daragh.king@genesys.com>
2025-09-01 11:22:14 +0100

    test(form-field-range): added Playwright tests
    added Playwright tests
    
    ✅ Closes: COMUI-4054

--- packages/genesys-spark-components/src/test/specTestUtils.ts
+++ packages/genesys-spark-components/src/test/specTestUtils.ts
@@ -1,7 +1,9 @@
-import { NewSpecPageOptions } from '@stencil/core/internal';
+import { NewSpecPageOptions, SpecPage } from '@stencil/core/internal';
 import { newSpecPage as stencilSpecPage } from '@stencil/core/testing';
 import MutationObserver from 'mutation-observer';
 import { RenderConfig } from './commonTestUtils';
+
+type ExtraActionsFn = (page: SpecPage) => Promise<void>;
 
 export async function newSpecPage(opts: NewSpecPageOptions) {
   global.MutationObserver = MutationObserver;
@@ -20,13 +22,16 @@ export async function newSpecPage(opts: NewSpecPageOptions) {
 export async function checkRenders(
   renderConfigs: RenderConfig[],
   components: unknown[],
-  language: string = 'en'
+  language: string = 'en',
+  extraActions: ExtraActionsFn = () => Promise.resolve()
 ) {
   renderConfigs.forEach(({ description, html }, index) => {
     it(
       description || `should render component as expected (${index + 1})`,
       async () => {
         const page = await newSpecPage({ components, html, language });
+
+        await extraActions(page);
 
         expect(page.root).toMatchSnapshot();
       }
