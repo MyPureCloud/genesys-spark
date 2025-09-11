3853d859e1b89ab251325fbdd77edc7249f15ee6
321gillian <gillian.middleton@gmail.com>
2025-09-11 16:38:31 +0100

    chore(radial-loading): Update tests and tidy example

--- packages/genesys-spark-components/src/components/stable/gux-radial-loading/tests/gux-radial-loading.spec.ts
+++ packages/genesys-spark-components/src/components/stable/gux-radial-loading/tests/gux-radial-loading.spec.ts
@@ -1,39 +1,10 @@
-import { newSpecPage } from '@test/specTestUtils';
 import { GuxRadialLoading } from '../gux-radial-loading';
+import { checkRenders } from '@test/specTestUtils';
+import { renderConfigs } from './gux-radial-loading.common';
+const components = [GuxRadialLoading];
 
 describe('gux-radial-loading', () => {
-  let component: GuxRadialLoading;
-
-  beforeEach(async () => {
-    const page = await newSpecPage({
-      components: [GuxRadialLoading],
-      html: `<gux-radial-loading></gux-radial-loading>`,
-      language: 'en'
-    });
-
-    component = page.rootInstance;
-  });
-
-  it('should build', async () => {
-    expect(component).toBeInstanceOf(GuxRadialLoading);
-  });
-
   describe('#render', () => {
-    [
-      '<gux-radial-loading screenreader-text="Loading"></gux-radial-loading>',
-      '<gux-radial-loading context="modal" screenreader-text="Loading"></gux-radial-loading>',
-      '<gux-radial-loading context="full-page" screenreader-text="Loading"></gux-radial-loading>',
-      '<gux-radial-loading context="input" screenreader-text="Loading"></gux-radial-loading>'
-    ].forEach((input, index) => {
-      it(`should render component as expected (${index + 1})`, async () => {
-        const page = await newSpecPage({
-          components: [GuxRadialLoading],
-          html: input,
-          language: 'en'
-        });
-
-        expect(page.root).toMatchSnapshot();
-      });
-    });
+    checkRenders(renderConfigs, components);
   });
 });
